b6f6a8d89d60be4c38bf74927dc7f1b089ba9aa9
Jingze Huang <huang.jingze@columbia.edu>
2018-07-14 12:02:24 -0700

    [New] DFS:Permutation

---
+++ src/LeetCode/DFS/PermutationWithDuplications.java
@@ -0,0 +1,60 @@
+package LeetCode.DFS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import utils.test.Print;
+
+public class PermutationWithDuplications {
+	
+	public List<List<Integer>> permuteUnique(int[] nums) {
+		List<List<Integer>> results = new ArrayList<>();
+		if (nums == null) {
+			return results;
+		}
+		int len = nums.length;
+		if (len == 0) {
+			return results;
+		}
+		Arrays.sort(nums);
+		Queue<Integer> queue = new LinkedList<Integer>();
+		for (int i = 0; i < len; i++) {
+			queue.offer(nums[i]);
+		}
+		dfs(queue,  new ArrayList<Integer>(), results);
+		
+		return  results;
+	}
+	
+	private void dfs(Queue<Integer> res, List<Integer> aResult, List<List<Integer>> results) {
+		if (res.isEmpty()) {
+			results.add(new ArrayList<Integer>(aResult));
+			return;
+		}
+		int head = res.peek();
+		for (int i = 0, len = res.size(); i < len; i++) {
+			Integer cursor = res.poll();
+			if (i == 0 || head != cursor) {
+				aResult.add(cursor);
+				dfs(res, aResult, results);
+				aResult.remove(aResult.size() - 1);
+				head = cursor;
+			}
+			res.offer(cursor);
+		}		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PermutationWithDuplications one = new PermutationWithDuplications();
+		int[] nums = {1, 2, 2, 3};
+		List<List<Integer>> list = one.permuteUnique(nums);
+		System.out.println(list.size());
+		for (int i = 0, len1 = list.size(); i < len1; i++) {
+			Print.printList(list.get(i), 0, list.get(i).size());
+		}
+	}
+}
---
+++ src/LeetCode/DFS/PermutationWithDuplicationsStandard.java
@@ -0,0 +1,44 @@
+package LeetCode.DFS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import utils.test.Print;
+
+public class PermutationWithDuplicationsStandard {
+	
+	public List<List<Integer>> permuteUnique(int[] nums) {
+	    List<List<Integer>> list = new ArrayList<>();
+	    Arrays.sort(nums);
+	    backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
+	    return list;
+	}
+
+	private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean [] used){
+	    if(tempList.size() == nums.length){
+	        list.add(new ArrayList<>(tempList));
+	    } else{
+	        for(int i = 0; i < nums.length; i++){
+	            if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue;
+	            used[i] = true; 
+	            tempList.add(nums[i]);
+	            backtrack(list, tempList, nums, used);
+	            used[i] = false; 
+	            tempList.remove(tempList.size() - 1);
+	        }
+	    }
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PermutationWithDuplicationsStandard one = new PermutationWithDuplicationsStandard();
+		int[] nums = {1, 2, 2, 3};
+		List<List<Integer>> list = one.permuteUnique(nums);
+		System.out.println(list.size());
+		for (int i = 0, len1 = list.size(); i < len1; i++) {
+			Print.printList(list.get(i), 0, list.get(i).size());
+		}
+	}
+	
+}
