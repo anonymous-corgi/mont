f81337813ad7ab46198211ead8693d56bbf393e9
Jingze Huang <huang.jingze@columbia.edu>
2018-07-15 15:24:41 -0700

    [New] NextTimePermutation

--- src/InterView/Google/OA/NextTime.java
+++ src/InterView/Google/OA/NextTime.java
@@ -5,23 +5,19 @@ import java.util.TreeSet;
 import utils.test.Test;
 
 public class NextTime {
-	
+		
     public String nextClosestTime(String time) {
         // write your code here
         if (time == null || time.length() != 5) {
             return time;
         }
         StringBuilder sb = new StringBuilder(time);
+        sb.delete(2, 3);
         TreeSet<Integer> set = new TreeSet<Integer>();
         int[] timeArr = new int[4];
-        int index = 0;
-        for (int i = 0; i < 5; i++) {
-        		if (i == 2) {
-        			continue;
-        		}
-        		timeArr[index] = Integer.parseInt(time.substring(i, i + 1));
-        		set.add(timeArr[index]);
-        		index++;
+        for (int i = 0; i < 4; i++) {
+        		timeArr[i] = Integer.parseInt(sb.substring(i, i + 1));
+        		set.add(timeArr[i]);
         }
         
         if (set.size() == 1) {
@@ -29,32 +25,36 @@ public class NextTime {
         }
         
         int lowest = set.first();
-        index = 3;
-        int[] higherNum = {3, 10, 6, 10};
         Integer cursor;
-        for (int i = 4; i >= 0; i--) {
-        		if (i == 2) {
-        			continue;
-        		}
-        		cursor = set.higher(timeArr[index]);
-        		if (cursor != null && cursor < higherNum[index]) {
+        for (int i = 3; i >= 0; i--) {
+        		cursor = set.higher(timeArr[i]);
+        		if (cursor != null) {
         			sb.replace(i, i + 1, "" + cursor);
-        			return sb.toString();
-        		} else {
-        			sb.replace(i, i + 1, "" + lowest);
+        			if (isValid(sb, i)) {
+        				sb.insert(2, ":");
+        				return sb.toString();        				
+        			}
         		}
-        		index--;
+        		sb.replace(i, i + 1, "" + lowest);
         }
+		sb.insert(2, ":");
         return sb.toString();
+    }
+    
+    private boolean isValid(StringBuilder sb, int i){
+    	if (i / 2 < 1) {
+    		return Integer.parseInt(sb.substring(0, 2)) < 24;
+    	} else {
+    		return Integer.parseInt(sb.substring(2, 4)) < 60;
+    	}
     }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		NextTime one = new NextTime();
-		String[] tests = {"15:59"};
-		String[] expecteds = {"23:55", "22:52", "21:22"};
+		String[] tests = {"15:59", "10:11", "01:21"};
+		String[] expecteds = {"19:11", "11:00", "01:22"};
 		for (int i = 0, len = tests.length; i < len; i++) {
-			Test.assertEquals(expecteds[i], one.nextClosestTime(tests[i]));
+			Test.assertEquals(expecteds[i], new NextTime().nextClosestTime(tests[i]));
 		}
 
 	}
---
+++ src/InterView/Google/OA/NextTimePermutation.java
@@ -0,0 +1,75 @@
+package InterView.Google.OA;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import utils.test.Test;
+
+public class NextTimePermutation {
+	
+	private String result;
+	private int oriMins;
+	private int minDiff = Integer.MAX_VALUE;
+	
+	public String nextClosestTime(String time) {
+		if (time == null || time.length() != 5) {
+			return time;
+		}
+		HashSet<Integer> set = new HashSet<>();
+		StringBuilder sb = new StringBuilder(time);
+		sb.delete(2, 3);
+		for (int i = 0; i < 4; i++) {
+			set.add(Integer.parseInt(sb.substring(i, i + 1)));
+		}
+		result = time;
+		if (set.size() == 1) {
+			return result;
+		}
+		oriMins = Integer.parseInt(sb.substring(0, 2)) * 60 + Integer.parseInt(sb.substring(2, 4));
+//		StringBuilder sBuilder = new
+		dfs(set, 0, new int[4]);
+		return result;
+	}
+	
+	private void dfs(Set<Integer> set, int position, int[] t) {
+		if (position == 4) {
+			int mins = countMin(t);
+			int diff = mins >= oriMins ? mins - oriMins : 1440 - oriMins + mins;
+			if (diff != 0 && diff < minDiff) {
+				minDiff = diff;
+				result = "" + t[0] + t[1] + ":" +t[2] + t[3];
+			}
+			return;
+		}
+		
+		for (Integer each : set) {
+			if (position == 0 && each > 2) {
+				continue;
+			}
+			if (position == 1 && (t[0] * 10 + each) > 23) {
+				continue;
+			}
+			if (position == 2 && each > 5) {
+				continue;
+			}
+			t[position] = each;
+			dfs(set, position + 1, t);
+		}
+	}
+	
+	private int countMin(int[] t) {
+		return (t[0] * 10 + t[1]) * 60 + t[2] * 10 + t[3];
+	}
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] tests = {"15:59", "10:11", "01:21"};
+		String[] expecteds = {"19:11", "11:00", "01:22"};
+				for (int i = 0, len = tests.length; i < len; i++) {
+			Test.assertEquals(expecteds[i], new NextTimePermutation().nextClosestTime(tests[i]));
+		}
+
+
+	}
+
+}
