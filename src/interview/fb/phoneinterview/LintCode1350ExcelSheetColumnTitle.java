d8a40bfadc738c0ab076a64367329a039ce7fc1d
Jingze Huang <huang.jingze@columbia.edu>
2018-07-18 22:14:40 -0700

    [New] LintCode1350

---
+++ src/interview/fb/phoneinterview/LintCode1350ExcelSheetColumnTitle.java
@@ -0,0 +1,47 @@
+package interview.fb.phoneinterview;
+
+public class LintCode1350ExcelSheetColumnTitle {
+  //This problem is hard because the start index is different: A-Z is 0-base But 1-26 is 1-base
+  public String convertToTitle(int n) {
+    // write your code here
+    if (n < 1) {
+      return "";
+    }
+    StringBuilder sb = new StringBuilder();
+    while (n > 0) {
+      n--;
+      char c = (char) ('A' + n % 26);
+      sb.insert(0, c);
+      n /= 26;
+    }
+    return sb.toString();
+  }
+  
+  public int converToInt(String str) {
+    if (str == null) {
+      return 0;
+    }
+    int sLen = str.length();
+    if (sLen == 0) {
+      return 0;
+    }
+    int i = 0;
+    int r = 0;
+    while (i < sLen) {
+      r *= 26;
+      r += str.charAt(i) - 'A' + 1; 
+      i++;
+    }
+    return r;
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    LintCode1350ExcelSheetColumnTitle one = new LintCode1350ExcelSheetColumnTitle();
+    int num = 26;
+    String sNum = one.convertToTitle(num);
+    int sInt = one.converToInt(sNum);
+    System.out.println("Input Num: " + num + ". Output string: " + sNum + ", int: " + sInt + ".");
+  }
+
+}
