c4e0ff10c64356acb826d69fcec28ce4a4681f52
Jingze Huang <huang.jingze@columbia.edu>
2018-07-20 02:00:10 -0700

    [New] Interval Base Class

---
+++ src/basicclass/Interval.java
@@ -0,0 +1,13 @@
+package basicclass;
+
+public class Interval {
+  
+  public int start;
+  public int end;
+  
+  public Interval(int start, int end) {
+    this.start = start;
+    this.end = end;
+  }
+
+}
