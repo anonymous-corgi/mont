a158b2f549610e2d81ef70a628d2805520a0cb74
Jingze Huang <huang.jingze@columbia.edu>
2018-07-22 17:12:53 -0700

    [Mod] BackPackVI

--- src/jiuzhang/dp/backpack/BackPackVI.java
+++ src/jiuzhang/dp/backpack/BackPackVI.java
@@ -2,7 +2,7 @@ package jiuzhang.dp.backpack;
 
 /**
  * LintCode 564. Combination Sum IV
- * 
+ * LeetCode 377. Combination Sum IV
  * Description
  * Given an integer array nums with all positive numbers and no duplicates, 
  * find the number of possible combinations that add up to a positive integer target.
@@ -20,28 +20,25 @@ package jiuzhang.dp.backpack;
  */
 public class BackPackVI {
 	//Count How many ways can get to target with repeated use
-    public int backPackVI(int[] nums, int target) {
-        // write your code here
-    		if (nums == null) {
-    			return 0;
-    		}
-    		int nLen = nums.length;
-    		if (nLen == 0) {
-    			return 0;
-    		}
-    		int[] dp = new int[target + 1];
-    		dp[0] = 1;
-    		for (int i = 1; i <= target; i++) {
-    			//j starts from 0, because it contains duplication.
-    			for (int j = 0; j < nLen; j++) {
-    				if (nums[j] > i) {
-    					continue;
-    				}
-    				dp[i] += dp[i - nums[j]];
-    			}
-    		}
-    		return dp[target];
-    }
+	public int backPackVI(int[] nums, int target) {
+		// write your code here
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		int nLen = nums.length;
+		int[] dp = new int[target + 1];
+		dp[0] = 1;
+		for (int i = 1; i <= target; i++) {
+			//j starts from 0, because it contains duplication.
+			for (int j = 0; j < nLen; j++) {
+				if (nums[j] > i) {
+					continue;
+				}
+				dp[i] += dp[i - nums[j]];
+			}
+		}
+		return dp[target];
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
