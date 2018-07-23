fe6ceb6ffbbc3108394998f5bc43609b411be7d6
Jingze Huang <huang.jingze@columbia.edu>
2018-07-22 18:56:55 -0700

    [New] LeetCode322CoinChange

---
+++ src/jiuzhang/dp/backpack/LeetCode322CoinChange.java
@@ -0,0 +1,29 @@
+package jiuzhang.dp.backpack;
+
+public class LeetCode322CoinChange {
+	
+	public int coinChange(int[] coins, int amount) {
+		if (coins == null || coins.length == 0) {
+			return -1;
+		}
+		int cLen = coins.length;
+		int[] dp = new int[amount + 1];
+		for (int i = 1; i <= amount; i++) {
+			dp[i] = -1;
+			for (int j = 0; j < cLen; j++) {
+				if (i >= coins[j] && dp[i - coins[j]] != -1) {
+					if (dp[i] == -1 || dp[i - coins[j]] + 1 < dp[i]) {
+						dp[i] = dp[i - coins[j]] + 1;
+					}
+				}
+			}
+		}
+		return dp[amount];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
