addd8cc37ab44b91d44b54f76373b3852002f8b4
Jingze Huang <huang.jingze@columbia.edu>
2018-07-28 10:36:20 -0700

    [New] LeetCode083RemoveDuplicatesFromSortedList

---
+++ src/leetcode/p001to100/LeetCode083RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,27 @@
+package leetcode.p001to100;
+
+import basicclass.ListNode;
+
+public class LeetCode083RemoveDuplicatesFromSortedList {
+  
+  public ListNode deleteDuplicates(ListNode head) {
+    ListNode slow = head;
+    ListNode fast;
+    while (slow != null) {
+      fast = slow.next;
+      while (fast != null && slow.val == fast.val) {
+        fast = fast.next;
+        slow.next.next = null;
+        slow.next = fast;
+      }
+      slow = fast;
+    }
+    return head;
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
