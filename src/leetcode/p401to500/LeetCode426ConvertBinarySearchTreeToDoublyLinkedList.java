dd2041af730a9c236f122a4dfc074a6cf780da9b
Jingze Huang <huang.jingze@columbia.edu>
2018-07-29 16:09:04 -0700

    [New] LeetCode426ConvertBinarySearchTreeToDoublyLinkedList

---
+++ src/leetcode/p401to500/LeetCode426ConvertBinarySearchTreeToDoublyLinkedList.java
@@ -0,0 +1,44 @@
+package leetcode.p401to500;
+
+import basicclass.DoublyListNode;
+import basicclass.TreeNode;
+
+public class LeetCode426ConvertBinarySearchTreeToDoublyLinkedList {
+
+  private DoublyListNode tail;
+  
+  public DoublyListNode bstToDoublyList(TreeNode root) {
+      // write your code here
+      if (root == null) {
+          return null;
+      }
+      DoublyListNode dummy = new DoublyListNode(-1);
+      tail = dummy;
+      helper(root);
+      dummy = dummy.next;
+      dummy.prev.next = null;
+      dummy.prev = null;
+      return dummy;
+  }
+  
+  private void helper(TreeNode root) {
+      if (root == null) {
+          return;
+      }
+      helper(root.left);
+      addToTail(new DoublyListNode(root.val));
+      helper(root.right);
+  }
+  
+  private void addToTail(DoublyListNode node) {
+      tail.next = node;
+      node.prev = tail;
+      tail = node;
+  }
+  
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
