6e60854308f9ab3241de690ee8a0fbefe49364b0
Jingze Huang <huang.jingze@columbia.edu>
2018-07-29 16:08:28 -0700

    [New] DoublyListNode

---
+++ src/basicclass/DoublyListNode.java
@@ -0,0 +1,15 @@
+package basicclass;
+
+public class DoublyListNode {
+  
+  public int val;
+  public DoublyListNode next;
+  public DoublyListNode prev;
+
+  public DoublyListNode(int val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+  
+}
