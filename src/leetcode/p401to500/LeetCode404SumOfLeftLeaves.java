013d694767571ede25bc1f7fb88f733ca63d74cd
Jingze Huang <huang.jingze@columbia.edu>
2018-07-29 17:04:23 -0700

    [New] LeetCode404SumOfLeftLeaves

---
+++ src/leetcode/p401to500/LeetCode404SumOfLeftLeaves.java
@@ -0,0 +1,34 @@
+package leetcode.p401to500;
+
+import basicclass.TreeNode;
+
+public class LeetCode404SumOfLeftLeaves {
+  
+  public int sumOfLeftLeaves(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    return helper(root, false);
+  }
+  
+  private int helper(TreeNode root, Boolean isLeft) {
+    if (root.left == null && root.right == null
+        && isLeft) {
+      return root.val;
+    }
+    int sum = 0;
+    if (root.left != null) {
+      sum += helper(root.left, true);
+    }
+    if (root.right != null) {
+      sum += helper(root.right, false);
+    }
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
