d129da5f7fb708eb8f5f4fe3641780266aa535a6
Jingze Huang <huang.jingze@columbia.edu>
2018-07-29 19:47:23 -0700

    [New] LeetCode060PermutationSequence

---
+++ src/leetcode/p001to100/LeetCode060PermutationSequence.java
@@ -0,0 +1,35 @@
+package leetcode.p001to100;
+
+import java.util.LinkedList;
+
+public class LeetCode060PermutationSequence {
+  
+  public String getPermutation(int n, int k) {
+    if (n < 1 || k < 1) {
+      return "";
+    }
+    
+    int pos = 1;
+    LinkedList<Integer> source = new LinkedList<>();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= n; i++) {
+      pos *= i;
+      source.add(i);
+    }
+    k--;
+    int digitIndex = n;
+    while (digitIndex > 0) {
+      pos /= digitIndex--;
+      int i = k / pos;
+      k %= pos;
+      sb.append(source.remove(i));
+    }
+    return sb.toString();
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
