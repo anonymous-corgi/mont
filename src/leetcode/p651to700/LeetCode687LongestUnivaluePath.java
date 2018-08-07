42c716740a106c6f457dbe1cbcebcc9400e84273
Jingze Huang <huang.jingze@columbia.edu>
2018-08-07 16:41:33 -0700

    [New] LeetCode687LongestUnivaluePath

---
+++ src/leetcode/p651to700/LeetCode687LongestUnivaluePath.java
@@ -0,0 +1,45 @@
+package leetcode.p651to700;
+
+import basicclass.TreeNode;
+
+public class LeetCode687LongestUnivaluePath {
+  
+  private int aMax = 0;
+  
+  public int longestUnivaluePath(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    helper(root);
+    return aMax;
+  }
+  
+  private int helper(TreeNode root) {
+    int tMax = 0;
+    int lMax = 0;
+    int rMax = 0;
+    if (root.left != null) {
+      lMax = helper(root.left);
+      if (root.left.val == root.val) {
+        tMax += ++lMax;
+      } else {
+          lMax = 0;
+      }
+    }
+    if (root.right != null) {
+      rMax = helper(root.right);
+      if (root.right.val == root.val) {
+        tMax += ++rMax;
+      } else {
+          rMax = 0;
+      }
+    }
+    aMax = Math.max(aMax, tMax);
+    return Math.max(lMax, rMax);
+  }
+
+  public static void main(String[] args) {
+    
+  }
+
+}
