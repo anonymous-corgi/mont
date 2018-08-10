9fa07396d22bd9c13c41ce69ec61f3aaafefc8e8
Jingze Huang <huang.jingze@columbia.edu>
2018-08-10 10:50:02 -0700

    [New] LeetCode505TheMazeII

--- src/leetcode/p501to550/LeetCode505TheMazeII.java
+++ src/leetcode/p501to550/LeetCode505TheMazeII.java
@@ -1,31 +1,32 @@
-package leetcode.p451to500;
+package leetcode.p501to550;
 
-public class LeetCode490TheMaze {
+public class LeetCode505TheMazeII {
   
   private final int[][][] DIRS = {{{-1, 0}, {1, 0}}, {{0, 1}, {0, -1}}};
   private int rows;
   private int cols;
-  public boolean hasPath(int[][] maze, int[] start, int[] des) {
-    // write your code here
+  public int shortestDistance(int[][] maze, int[] start, int[] des) {
     rows = maze.length;
     cols = maze[0].length;
-    dfs(maze, start[0], start[1], 0);
-    dfs(maze, start[0], start[1], 1);
-    return maze[des[0]][des[1]] == -1;
+    dfs(maze, start[0], start[1], 0, -1);
+    dfs(maze, start[0], start[1], 1, -1);
+    return -maze[des[0]][des[1]] - 1;
   }
   
-  private void dfs(int[][] maze, int sx, int sy, int d) {
-    maze[sx][sy] = -1; 
+  private void dfs(int[][] maze, int sx, int sy, int d, int dis) {
+    maze[sx][sy] = dis; 
     for (int[] dir : DIRS[d]) {
       int nx = sx;
       int ny = sy;
+      int nDis = dis;
       while (inBound(nx + dir[0], ny + dir[1]) 
           && maze[nx + dir[0]][ny + dir[1]] != 1) {
         nx = nx + dir[0];
         ny = ny + dir[1];
+        nDis--;
       }
-      if (maze[nx][ny] != -1) {
-        dfs(maze, nx, ny, (d + 1) % 2);
+      if (maze[nx][ny] == 0 || nDis > maze[nx][ny]) {
+        dfs(maze, nx, ny, (d + 1) % 2, nDis);
       }
     }
   }
@@ -36,7 +37,7 @@ public class LeetCode490TheMaze {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    LeetCode490TheMaze one = new LeetCode490TheMaze();
+    LeetCode505TheMazeII one = new LeetCode505TheMazeII();
     int[][] maze = {{1,0,1,1},{0,0,0,1},{0,0,0,0},{1,0,0,1}};
     int[] start = {0,1};
     int[] des = {2,3};
@@ -44,7 +45,7 @@ public class LeetCode490TheMaze {
 //    int[][] maze = {{0,0,1,0,0},{0,0,0,0,0},{0,0,0,1,0},{1,1,0,1,1},{0,0,0,0,0}};
 //    int[] start = {0,4};
 //    int[] des = {3,2};
-    System.out.println(one.hasPath(maze, start, des));
+    System.out.println(one.shortestDistance(maze, start, des));
   }
 
 }
