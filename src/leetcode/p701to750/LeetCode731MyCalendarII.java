b8a2349f539a84d283b8b66614141e84e45ac705
Jingze Huang <huang.jingze@columbia.edu>
2018-08-11 15:48:56 -0700

    [Mod] Calendar

--- src/leetcode/p701to750/LeetCode731MyCalendarII.java
+++ src/leetcode/p701to750/LeetCode731MyCalendarII.java
@@ -1,13 +1,33 @@
 package leetcode.p701to750;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class LeetCode731MyCalendarII {
   
-  private List<int[]> books = new ArrayList<>();   
-  
+  //Method 1:
+  private TreeMap<Integer, Integer> map = new TreeMap<>();
   public boolean book(int s, int e) {
+    map.put(s, map.getOrDefault(s, 0) + 1);
+    map.put(e, map.getOrDefault(e, 0) - 1);
+    int cnt = 0;
+    for (Integer v : map.values()) {
+      cnt += v;
+      if (cnt == 3) {
+        map.put(s, map.get(s) - 1);
+        map.put(e, map.get(e) + 1);
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  //Method 2:
+  private Set<int[]> books = new HashSet<>();   
+  public boolean book2(int s, int e) {
       MyCalendar overlaps = new MyCalendar();
       for (int[] b : books)
           if (Math.max(b[0], s) < Math.min(b[1], e)) {
@@ -31,8 +51,12 @@ public class LeetCode731MyCalendarII {
   }
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    LeetCode731MyCalendarII one =
+        new LeetCode731MyCalendarII();
+    int[][] times = {{10, 11}, {10, 13}, {5, 6}, {3, 6}, {4, 13}, {1, 16}};
+    for (int i = 0; i < times.length; i++) {
+      System.out.println(one.book(times[i][0], times[i][1]));
+    }
   }
 
 }
--- src/leetcode/p701to750/LeetCode732MyCalendarIII.java
+++ src/leetcode/p701to750/LeetCode732MyCalendarIII.java
@@ -6,18 +6,23 @@ public class LeetCode732MyCalendarIII {
   
   private TreeMap<Integer, Integer> timeline = new TreeMap<>();
   public int book(int s, int e) {
-    timeline.put(s, timeline.getOrDefault(s, 0) + 1); // 1 new event will be starting at [s]
-    timeline.put(e, timeline.getOrDefault(e, 0) - 1); // 1 new event will be ending at [e];
+    timeline.put(s, timeline.getOrDefault(s, 0) + 1);
+    timeline.put(e, timeline.getOrDefault(e, 0) - 1);
     int count = 0;
     int res = 0;
-    for (int v : timeline.values())
+    for (int v : timeline.values()) {
       res = Math.max(res, count += v);
+    }
     return res;
   }
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    LeetCode732MyCalendarIII one =
+        new LeetCode732MyCalendarIII();
+    int[][] times = {{10, 11}, {10, 13}, {5, 6}, {3, 6}, {4, 13}, {1, 16}};
+    for (int i = 0; i < times.length; i++) {
+      System.out.println(one.book(times[i][0], times[i][1]));
+    }
   }
 
 }
