b9155e97c848a78b2ca0e6b04c8719c447c62fd9
Jingze Huang <huang.jingze@columbia.edu>
2018-08-14 15:47:58 -0700

    [Mod] LeetCode489RobotRoomCleaner

--- src/leetcode/p451to500/LeetCode489RobotRoomCleaner.java
+++ src/leetcode/p451to500/LeetCode489RobotRoomCleaner.java
@@ -12,27 +12,31 @@ public class LeetCode489RobotRoomCleaner {
   private int x;
   private int y;
   private int dir = 0;
+  //Map<Integer, Set<Integer>> is a good data structure for unknown boundary place.
   private Map<Integer, Set<Integer>> map = new HashMap<>();
   
   public void cleanRoom(Robot robot) {
-    Set<Integer> set =  map.getOrDefault(x, new HashSet<Integer>());
-    if (set.contains(y)) {
-      return;
-    }
-    set.add(y);
-    map.put(x, set);
+    //Each time calling cleanRoom means you are currently in (x, y).
+    Set<Integer> ySet =  map.getOrDefault(x, new HashSet<Integer>());
+    ySet.add(y);
+    map.put(x, ySet);
     robot.clean();
     
     for (int i = 0; i < 4; i++) {
       int nx = x + DIRS[dir][0];
       int ny = y + DIRS[dir][1];
       if ((!map.containsKey(nx) || !map.get(nx).contains(ny)) && robot.move()) {
+        //Update the coordinate right after you move forward. And then clean the room.
         x = nx;
         y = ny;
         cleanRoom(robot);
+        //Because the direction has turn 180 degree After the cleanRoom() is called,
+        //we just need to call move() to move back to the original position.
         robot.move();      
         x += DIRS[dir][0];
         y += DIRS[dir][1];
+        //If this is not the 4th Direction, we turn left to clean the next direction.
+        //Otherwise, we turn right to make sure we turn 180 degree to the direction we just enter.
         if (i != 3) {
           turnLeft(robot);
         } else {
