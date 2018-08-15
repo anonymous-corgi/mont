2458a56e2d82cc3151e4713691c066d7019a3c61
Jingze Huang <huang.jingze@columbia.edu>
2018-08-15 11:34:42 -0700

    [Mod] LeetCode338CountingBits

--- src/leetcode/p301to350/LeetCode338CountingBits.java
+++ src/leetcode/p301to350/LeetCode338CountingBits.java
@@ -2,28 +2,36 @@ package leetcode.p301to350;
 
 public class LeetCode338CountingBits {
   
-  public int[] countBits(int num) {
-    int[] dp = new int[num + 1];
-    for (int i = 0; i <= num; i++) {
-      dp[i] = dp[i >> 1] + (i & 1); 
+  public static class MoveBit_method {
+    
+    public int[] countBits(int num) {
+      int[] dp = new int[num + 1];
+      for (int i = 0; i <= num; i++) {
+        dp[i] = dp[i >> 1] + (i & 1); 
+      }
+      return dp;
     }
-    return dp;
+    
   }
   
-  public int[] countBits2(int num) {
-    int[][] dp = new int[2][num + 1];
-    for (int i = 1; i <= num; i++) {
-      dp[1][i] = (i & (i - 1));
-      dp[0][i] = dp[0][dp[1][i]] + 1;
+  public static class BitOP_method {
+    
+    public int[] countBits(int num) {
+      int[] dp = new int[num + 1];
+      for (int i = 1; i <= num; i++) {
+        dp[i] = dp[i & (i - 1)] + 1;
+      }
+      return dp;
     }
-    return dp[0];
+    
   }
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    LeetCode338CountingBits one = new LeetCode338CountingBits();
+    LeetCode338CountingBits.BitOP_method one = 
+        new LeetCode338CountingBits.BitOP_method();
     int num = 10;
-    System.out.println(one.countBits2(num));
+    System.out.println(one.countBits(num));
   }
 
 }
