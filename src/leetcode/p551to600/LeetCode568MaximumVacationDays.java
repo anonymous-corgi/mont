9adfa678ce80fced79d727566ad3e86fe0aa4188
Jingze Huang <huang.jingze@columbia.edu>
2018-08-15 21:14:24 -0700

    [Mod] LeetCode568MaximumVacationDays

--- src/leetcode/p551to600/LeetCode568MaximumVacationDays.java
+++ src/leetcode/p551to600/LeetCode568MaximumVacationDays.java
@@ -3,25 +3,30 @@ package leetcode.p551to600;
 public class LeetCode568MaximumVacationDays {
   
   public int maxVacationDays(int[][] flights, int[][] days) {
-    int k = days[0].length;
-    int n = days.length;
-    int[][] dpMax = new int[n][k];
-    for (int i = 0; i < n; i++) {
-      dpMax[i][k - 1] = days[i][k - 1];
+    int weeks = days[0].length;
+    int citys = days.length;
+    int[] preDp = new int[citys];
+    int[] curDp = new int[citys];
+    for (int city = 0; city < citys; city++) {
+      curDp[city] = days[city][weeks - 1];
     }
-    for (int week = k - 2; week >= 0; week--) {
-      for (int city = 0; city < n; city++) {
-        dpMax[city][week] = days[city][week]
-            + getMaxDays(dpMax, city, week + 1, flights);
+    for (int week = weeks - 2; week >= 0; week--) {
+      int[] temp = preDp;
+      preDp = curDp;
+      curDp = temp;
+      for (int city = 0; city < citys; city++) {
+        curDp[city] = days[city][week]
+            + getMaxDays(preDp, city, flights);
       }
     }
-    return getMaxDays(dpMax, 0, 0, flights);
+    return getMaxDays(curDp, 0, flights);
   }
-  public int getMaxDays(int[][] dp, int city, int week, int[][] flights) {
-    int max = dp[city][week];
+  
+  public int getMaxDays(int[]dp, int city, int[][] flights) {
+    int max = dp[city];
     for (int i = 0; i < flights.length; i++) {
       if (flights[city][i] == 1) {
-        max = Math.max(max, dp[i][week]);
+        max = Math.max(max, dp[i]);
       }
     }
     return max;
