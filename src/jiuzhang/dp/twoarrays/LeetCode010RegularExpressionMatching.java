4110bda0209dbe7be315df2fda315dd2c568d97e
Jingze Huang <huang.jingze@columbia.edu>
2018-08-16 12:05:54 -0700

    [Mod] LeetCode010RegularExpressionMatching

--- src/jiuzhang/dp/twoarrays/LeetCode010RegularExpressionMatching.java
+++ src/jiuzhang/dp/twoarrays/LeetCode010RegularExpressionMatching.java
@@ -3,7 +3,6 @@ package jiuzhang.dp.twoarrays;
 public class LeetCode010RegularExpressionMatching {
   
   public boolean isMatch(String s, String p) {
-    // write your code here
     if (s == null || p == null) {
       return false;
     }
@@ -15,13 +14,18 @@ public class LeetCode010RegularExpressionMatching {
     for (int i = 0; i < isMatch.length; i++) {
       for (int j = 1; j < isMatch[0].length; j++) {
         if (j > 1 && ps[j - 1] == '*') {
+          //This is the situation neglecting the last two chars of p;
           isMatch[i][j] |= isMatch[i][j - 2]; 
           
+          //This is the situation where the last char of s 
+          //and the last second char of p match.
           if (i > 0 && (ss[i - 1] == ps[j - 2]
               || ps[j - 2]== '.')) {
             isMatch[i][j] |= isMatch[i - 1][j];
           }
-        } else if (i > 0 && (ss[i - 1] == ps[j - 1]
+        } 
+        //This is the situation where last chars of both s and p match.
+        else if (i > 0 && (ss[i - 1] == ps[j - 1]
             || ps[j - 1] == '.')) {
           isMatch[i][j] |= isMatch[i - 1][j - 1];
         }
@@ -31,10 +35,10 @@ public class LeetCode010RegularExpressionMatching {
   }
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    LeetCode010RegularExpressionMatching one = new LeetCode010RegularExpressionMatching();
-    String s = "aaaa";
-    String p = "aa*";
+    String s = "aa";
+    String p = "a*";
+    LeetCode010RegularExpressionMatching one = 
+        new LeetCode010RegularExpressionMatching();
     System.out.println(one.isMatch(s, p));
   }
 
