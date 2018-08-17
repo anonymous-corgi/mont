9f2e1b3b42170e548af4bc601311de9c01e9eff2
Jingze Huang <huang.jingze@columbia.edu>
2018-08-17 15:03:38 -0700

    [New] LeetCode150EvaluateReversePolishNotation

---
+++ src/leetcode/p101to150/LeetCode150EvaluateReversePolishNotation.java
@@ -0,0 +1,42 @@
+package leetcode.p101to150;
+
+import java.util.Stack;
+
+public class LeetCode150EvaluateReversePolishNotation {
+  
+  public int evalRPN(String[] tokens) {
+    Stack<Integer> stack = new Stack<>();
+    for (int i = 0; i < tokens.length; i++) {
+      if ("+".equals(tokens[i])) {
+        int b = stack.pop();
+        int f = stack.pop();
+        int res = f + b;
+        stack.push(res);
+      } else if ("-".equals(tokens[i])) {
+        int b = stack.pop();
+        int f = stack.pop();
+        int res = f - b;
+        stack.push(res);
+      } else if ("*".equals(tokens[i])) {
+        int b = stack.pop();
+        int f = stack.pop();
+        int res = f * b;
+        stack.push(res);
+      } else if ("/".equals(tokens[i])) {
+        int b = stack.pop();
+        int f = stack.pop();
+        int res = f / b;
+        stack.push(res);
+      } else {
+        int res = Integer.parseInt(tokens[i]);
+        stack.push(res);
+      }
+    }
+    return stack.pop();
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
