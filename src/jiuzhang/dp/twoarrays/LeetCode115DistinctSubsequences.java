ca0d5f1c8c940c85e0d1ba7f136661f7d669af32
Jingze Huang <huang.jingze@columbia.edu>
2018-08-17 14:02:18 -0700

    [Mod] LeetCode115DistinctSubsequences

--- src/jiuzhang/dp/twoarrays/LeetCode115DistinctSubsequences.java
+++ src/jiuzhang/dp/twoarrays/LeetCode115DistinctSubsequences.java
@@ -6,15 +6,12 @@ public class LeetCode115DistinctSubsequences {
     if (S == null || T == null) {
       return 0;
     }
-    
     int sLen = S.length() + 1;
     int tLen = T.length() + 1;
     int[][] dp = new int[sLen][tLen];
-    
-    for (int i = 0; i < sLen; i++) {
-      dp[i][0] = 1;
-    }
+    dp[0][0] = 1;
     for (int i = 1; i < sLen; i++) {
+      dp[i][0] = 1;
       for (int j = 1; j < tLen; j++) {
         dp[i][j] = dp[i - 1][j];
         if (S.charAt(i - 1) == T.charAt(j - 1)) {
@@ -22,9 +19,8 @@ public class LeetCode115DistinctSubsequences {
         }
       }
     }
-    return dp[sLen][tLen];
+    return dp[sLen - 1][tLen - 1];
   }
-  
   /**
    * In point (4, 2)
    * d[3][2] means "abc" has d[3][2] "ab" not using the second "b"
