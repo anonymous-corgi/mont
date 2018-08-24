5d7390d3e5a3911d4c43ddba5920265b7f9d9568
Jingze Huang <huang.jingze@columbia.edu>
2018-08-24 11:12:20 -0700

    [New] LeetCode870AdvantageShuffle

---
+++ src/leetcode/p851to900/LeetCode870AdvantageShuffle.java
@@ -0,0 +1,33 @@
+package leetcode.p851to900;
+
+import java.util.Arrays;
+
+public class LeetCode870AdvantageShuffle {
+  
+  public int[] advantageCount(int[] A, int[] B) {
+    int[] res = new int[B.length];
+    int[][] bMap = new int[B.length][2];
+    for (int i = 0; i < B.length; i++) {
+      bMap[i][0] = B[i];
+      bMap[i][1] = i;
+    }
+    Arrays.sort(A);
+    Arrays.sort(bMap, (a, b) -> (a[0] - b[0]));
+    int left = 0;
+    int right = bMap.length - 1;
+    int bIndex = bMap.length - 1;
+    while (left <= right) {
+      if (A[right] > bMap[bIndex][0]) {
+        res[bMap[bIndex--][1]] = A[right--];
+      } else {
+        res[bMap[bIndex--][1]] = A[left++];
+      }
+    }
+    return res;
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
