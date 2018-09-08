10c57d6188836e49d9f3715184ae7a7011c9e24b
Jingze Huang <huang.jingze@columbia.edu>
2018-09-07 18:12:32 -0700

    [Mod] UnitTest Updated

--- src/interview/google/oa/LastTime.java
+++ src/interview/google/oa/LastTime.java
@@ -1,53 +1,53 @@
 package interview.google.oa;
 
+import static org.junit.Assert.*;
 import java.util.TreeSet;
-
-import utils.test.Test;
+import org.junit.Test;
 
 public class LastTime {
-	
+  
 //  /**Solution1: */		
-	public String lastClosestTime(String time) {
-        if (time == null || time.length() != 5) {
-            return time;
-        }
-        StringBuilder sb = new StringBuilder(time);
-        TreeSet<Integer> set = new TreeSet<Integer>();
-        int[] tArr = new int[4];
-        sb.delete(2, 3);
-        for (int i = 0; i < 4; i++) {
-            tArr[i] = Integer.parseInt(sb.substring(i, i + 1));
-            set.add(tArr[i]);
-        }
-        
-        if (set.size() == 1) {
-            return time;
-        }
-        
-        final int highest= set.last();        
-        for (int i = 3; i >= 0; i--) {
-            Integer lo = set.lower(tArr[i]);
-            if (lo != null) {
-                sb.replace(i, i + 1, "" + lo);
-                if (isValid(sb, i)) {                    
-                    sb.insert(2, ":");
-                    return sb.toString();
-                }
-            }
-            sb.replace(i, i + 1, "" + highest);
-        }
-        sb.insert(2, ":");
-        return sb.toString();
+  public String lastClosestTime(String time) {
+    if (time == null || time.length() != 5) {
+      return time;
     }
-	
-    private static boolean isValid(StringBuilder sb, int position) {
-        if (position / 2 == 0) {
-            return Integer.parseInt(sb.substring(0, 2)) < 24;
-        } else {
-            return Integer.parseInt(sb.substring(2, 4)) < 60;
-        }
+    StringBuilder sb = new StringBuilder(time);
+    TreeSet<Integer> set = new TreeSet<Integer>();
+    int[] tArr = new int[4];
+    sb.delete(2, 3);
+    for (int i = 0; i < 4; i++) {
+      tArr[i] = Integer.parseInt(sb.substring(i, i + 1));
+      set.add(tArr[i]);
     }
-
+    
+    if (set.size() == 1) {
+      return time;
+    }
+    
+    final int highest= set.last();        
+    for (int i = 3; i >= 0; i--) {
+      Integer lo = set.lower(tArr[i]);
+      if (lo != null) {
+        sb.replace(i, i + 1, "" + lo);
+        if (isValid(sb, i)) {                    
+          sb.insert(2, ":");
+          return sb.toString();
+        }
+      }
+      sb.replace(i, i + 1, "" + highest);
+    }
+    sb.insert(2, ":");
+    return sb.toString();
+  }
+  
+  private static boolean isValid(StringBuilder sb, int position) {
+    if (position / 2 == 0) {
+      return Integer.parseInt(sb.substring(0, 2)) < 24;
+    } else {
+      return Integer.parseInt(sb.substring(2, 4)) < 60;
+    }
+  }
+  
 //  /**Solution2: */	
 //	private int[] highestNum = {3, 4, 6, 10};
 //	
@@ -90,16 +90,15 @@ public class LastTime {
 //		}
 //		return sb.toString();
 //	}
-
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LastTime one = new LastTime();
-		String[] tests = {"23:59", "22:55", "22:11"};
-		String[] expecteds = {"23:55", "22:52", "21:22"};
-		for (int i = 0, len = tests.length; i < len; i++) {
-			Test.assertEquals(expecteds[i], one.lastClosestTime(tests[i]));
-		}
-	}
-	
+  
+  
+  @Test
+  public void testLastClosestTime() {
+    String[] tests = {"23:59", "22:55", "22:11"};
+    String[] expecteds = {"23:55", "22:52", "21:22"};
+    for (int i = 0, len = tests.length; i < len; i++) {
+      assertEquals(expecteds[i], lastClosestTime(tests[i]));
+    }
+  }
+  
 }
--- src/interview/google/oa/NextTime.java
+++ src/interview/google/oa/NextTime.java
@@ -1,62 +1,61 @@
 package interview.google.oa;
 
+import static org.junit.Assert.*;
 import java.util.TreeSet;
-
-import utils.test.Test;
+import org.junit.Test;
 
 public class NextTime {
-		
-    public String nextClosestTime(String time) {
-        // write your code here
-        if (time == null || time.length() != 5) {
-            return time;
-        }
-        StringBuilder sb = new StringBuilder(time);
-        sb.delete(2, 3);
-        TreeSet<Integer> set = new TreeSet<Integer>();
-        int[] timeArr = new int[4];
-        for (int i = 0; i < 4; i++) {
-        		timeArr[i] = Integer.parseInt(sb.substring(i, i + 1));
-        		set.add(timeArr[i]);
-        }
-        
-        if (set.size() == 1) {
-            return time;
-        }
-        
-        int lowest = set.first();
-        Integer cursor;
-        for (int i = 3; i >= 0; i--) {
-        		cursor = set.higher(timeArr[i]);
-        		if (cursor != null) {
-        			sb.replace(i, i + 1, "" + cursor);
-        			if (isValid(sb, i)) {
-        				sb.insert(2, ":");
-        				return sb.toString();        				
-        			}
-        		}
-        		sb.replace(i, i + 1, "" + lowest);
-        }
-		sb.insert(2, ":");
-        return sb.toString();
+  
+  public String nextClosestTime(String time) {
+    if (time == null || time.length() != 5) {
+      return time;
+    }
+    StringBuilder sb = new StringBuilder(time);
+    sb.delete(2, 3);
+    TreeSet<Integer> set = new TreeSet<Integer>();
+    int[] timeArr = new int[4];
+    for (int i = 0; i < 4; i++) {
+      timeArr[i] = Integer.parseInt(sb.substring(i, i + 1));
+      set.add(timeArr[i]);
     }
     
-    private boolean isValid(StringBuilder sb, int i){
-    	if (i / 2 < 1) {
-    		return Integer.parseInt(sb.substring(0, 2)) < 24;
-    	} else {
-    		return Integer.parseInt(sb.substring(2, 4)) < 60;
-    	}
+    if (set.size() == 1) {
+      return time;
     }
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String[] tests = {"15:59", "10:11", "01:21"};
-		String[] expecteds = {"19:11", "11:00", "01:22"};
-		for (int i = 0, len = tests.length; i < len; i++) {
-			Test.assertEquals(expecteds[i], new NextTime().nextClosestTime(tests[i]));
-		}
-
-	}
-
+    
+    int lowest = set.first();
+    Integer cursor;
+    for (int i = 3; i >= 0; i--) {
+      cursor = set.higher(timeArr[i]);
+      if (cursor != null) {
+        sb.replace(i, i + 1, "" + cursor);
+        if (isValid(sb, i)) {
+          sb.insert(2, ":");
+          return sb.toString();        				
+        }
+      }
+      sb.replace(i, i + 1, "" + lowest);
+    }
+    sb.insert(2, ":");
+    return sb.toString();
+  }
+  
+  private boolean isValid(StringBuilder sb, int i){
+    if (i / 2 < 1) {
+      return Integer.parseInt(sb.substring(0, 2)) < 24;
+    } else {
+      return Integer.parseInt(sb.substring(2, 4)) < 60;
+    }
+  }
+  
+  
+  @Test
+  public void testNextClosestTime() {
+    String[] tests = {"15:59", "10:11", "01:21"};
+    String[] expecteds = {"19:11", "11:00", "01:22"};
+    for (int i = 0, len = tests.length; i < len; i++) {
+      assertEquals(expecteds[i], nextClosestTime(tests[i]));
+    }
+  }
+  
 }
