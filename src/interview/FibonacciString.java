35d06d965cc936a384078b5bff59740b9875fa9c
Jingze Huang <huang.jingze@columbia.edu>
2018-09-07 17:47:54 -0700

    [Mod] TestCases Added

--- src/interview/FibonacciString.java
+++ src/interview/FibonacciString.java
@@ -1,5 +1,8 @@
 package interview;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class FibonacciString {
   
   public static class DFS_method {
@@ -14,14 +17,14 @@ public class FibonacciString {
       }
       
       for (int i = start + 1; i <= chs.length; i++) {      
-        int val = Integer.parseInt(String.valueOf(chs, start, i - start));
-        if (n > 2 && val > val1 + val2) {
+        long val = Long.parseLong(String.valueOf(chs, start, i - start));
+        if (n > 2 && (val > val1 + val2 || val > Integer.MAX_VALUE) ) {
           break;
         }
         if (n > 2 && val != val1 + val2) {
           continue;
         }
-        if (helper(chs, n + 1, i, val2, val)) {
+        if (helper(chs, n + 1, i, val2, (int) val)) {
           return true;
         }
       }
@@ -35,12 +38,15 @@ public class FibonacciString {
     public boolean isFibonacciString(String s) {
       
       for (int i = 1, iLen = s.length() / 2; i <= iLen; i++) {
-        int val1 = Integer.parseInt(s.substring(0, i));
+        long val1 = Long.parseLong(s.substring(0, i));
+        if (val1 > Integer.MAX_VALUE) { break; }
         for (int j = i + 1, jLen = s.length(); j <= jLen; j++) {
-          int val2 = Integer.parseInt(s.substring(i, j));
+          long val2 = Long.parseLong(s.substring(i, j));
+          if (val2 > Integer.MAX_VALUE) { break; }
           int n = j;
           for (int m = j + 1; m <= jLen; m++) {
-            int val = Integer.parseInt(s.substring(n, m));            
+            long val = Long.parseLong(s.substring(n, m));
+            if (val > Integer.MAX_VALUE) { break; }
             if (val > val1 + val2) {
               break;
             }
@@ -60,15 +66,21 @@ public class FibonacciString {
     
   }
   
-  
-  public static void main(String[] args) {
-    String s = "111223";
-    FibonacciString.DFS_method one = 
+  @Test 
+  public void testIsFibonacciString() {
+    FibonacciString.DFS_method dfs = 
         new FibonacciString.DFS_method();    
-    FibonacciString.NonRecursive_method two = 
+    FibonacciString.NonRecursive_method non = 
         new FibonacciString.NonRecursive_method();
-    System.out.println(one.isFibonacciString(s));
-    System.out.println(two.isFibonacciString(s));
+    
+    String[] ss = {"112358", "1123581321345589144", "10013113126239", 
+        "11", "12345", "1123581321345589143"};
+    
+    boolean[] ex = {true, true, true, false, false, false};
+    for (int i = 0; i < ss.length; i++) {
+      assertEquals(ex[i], dfs.isFibonacciString(ss[i]));
+      assertEquals(ex[i], non.isFibonacciString(ss[i]));
+    }
   }
-  
+
 }
