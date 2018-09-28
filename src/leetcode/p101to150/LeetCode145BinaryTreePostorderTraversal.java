f9fd5c77c69bebf06ebbc6cc4ea6fc476b5aa56d
Jingze Huang <huang.jingze@columbia.edu>
2018-09-28 13:15:06 -0700

    [New] LeetCode145BinaryTreePostorderTraversal

---
+++ src/leetcode/p101to150/LeetCode145BinaryTreePostorderTraversal.java
@@ -0,0 +1,62 @@
+package leetcode.p101to150;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+import basicclass.TreeNode;
+
+public class LeetCode145BinaryTreePostorderTraversal {
+  
+  public static class Reverse_Preorder_method {
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+      LinkedList<Integer> res = new LinkedList<>();
+      Stack<TreeNode> stack = new Stack<>();
+      while (!stack.isEmpty() || root != null) {
+        
+        if (root != null) {
+          res.addFirst(root.val); 
+          stack.push(root);
+          root = root.right;
+        } else {
+          root = stack.pop().left;
+        }
+        
+      }
+      
+      return res;
+    }
+    
+  }
+  
+  public static class Regular_method {
+    
+    //Post-order Postorder non-recursive
+    public List<Integer> postorderTraversal(TreeNode root) {
+      List<Integer> res = new ArrayList<>();
+      Stack<TreeNode> stack = new Stack<>();
+      TreeNode prev = null;
+      while (!stack.isEmpty() || root != null) {
+        
+        if (root != null) {
+          stack.push(root);
+          root = root.left;
+        } else {
+          if (stack.peek().right != null && stack.peek().right != prev) {
+            root = stack.peek().right;
+          } else {
+            res.add(stack.peek().val);
+            prev = stack.pop();
+          }
+        }
+        
+      }
+      
+      return res;
+    }
+    
+  }
+  
+
+}
