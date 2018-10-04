c3813768e2ebfbdf3d89b6007409c6fdade14f24
Jingze Huang <huang.jingze@columbia.edu>
2018-10-04 14:36:23 -0700

    [New] LeetCode112PathSum.java

---
+++ src/leetcode/p101to150/LeetCode112PathSum.java
@@ -0,0 +1,41 @@
+package leetcode.p101to150;
+
+import basicclass.TreeNode;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LeetCode112PathSum {
+  //Non-recursive Method
+  public boolean hasPathSum(TreeNode root, int sum) {
+    if (root == null) {
+      return false;
+    }
+    int count = 0;
+    TreeNode last = null;
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    while (!stack.isEmpty() || root != null) {
+      if (root != null) {
+        count += root.val;
+        stack.push(root);
+        root = root.left;
+      } else {
+        TreeNode prev = stack.pop();
+        if (prev.right == null) {
+          if (prev.left == null && count == sum) {
+            return true;
+          }
+          count -= prev.val;
+          last = prev;
+        } else if (prev.right == last) {
+          count -= prev.val;
+          last = prev;
+        } else {
+          stack.push(prev);
+          root = prev.right;
+        }
+      }
+    }
+    return false;
+  }
+
+}
