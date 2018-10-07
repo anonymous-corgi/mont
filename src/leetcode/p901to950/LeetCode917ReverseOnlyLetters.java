d59798cea0ad2fa323bf8018ac37467a41383af1
Jingze Huang <huang.jingze@columbia.edu>
2018-10-06 22:15:49 -0700

    [New] LeetCode917ReverseOnlyLetters

---
+++ src/leetcode/p901to950/LeetCode917ReverseOnlyLetters.java
@@ -0,0 +1,38 @@
+package leetcode.p901to950;
+
+public class LeetCode917ReverseOnlyLetters {
+
+  interface Method {
+    String reverseOnlyLetters(String s);
+  }
+
+  public static class My_method implements Method{
+
+    @Override
+    public String reverseOnlyLetters(String s) {
+      int l = 0;
+      int r = s.length() - 1;
+      char[] res = s.toCharArray();
+      while (l < r) {
+        while (l < r && !Character.isAlphabetic(res[l])) {
+          l++;
+        }
+        while (l < r && !Character.isAlphabetic(res[r])) {
+          r--;
+        }
+        if (l < r) {
+          swap(res, l++, r--);
+        }
+      }
+      return String.valueOf(res);
+    }
+
+    private void swap(char[] chs, int a, int b) {
+      char temp = chs[a];
+      chs[a] = chs[b];
+      chs[b] = temp;
+    }
+
+  }
+
+}
