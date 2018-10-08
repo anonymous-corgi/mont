dc8a27d2debd43d1619c83ba49e62d086981eb63
Jingze Huang <huang.jingze@columbia.edu>
2018-10-07 18:36:24 -0700

    [New] LintCode089KSum

--- src/jiuzhang/dp/backpack/LintCode089KSum.java
+++ src/jiuzhang/dp/backpack/LintCode089KSum.java
@@ -16,36 +16,28 @@ package jiuzhang.dp.backpack;
  * Return 2.
  */
 public class LintCode089KSum {
+
   public int kSum(int[] A, int k, int target) {
     // write your code here
-    if (A == null || A.length == 0) {
+    if (A == null || A.length == 0 || target == 0) {
       return 0;
     }
-    if (target == 0) {
-      return 0;
-    }
-    int aLen = A.length;
+
     int[][] dp = new int[k + 1][target + 1];
-    //Because all A[] are >= 0, and at least have 1 item,
-    //It doesn't exist situation where the sum is 0.
-    //There is no need to set dp[i][0] to 1.
-    for (int i = 0; i < aLen; i++) {
-      if (A[i] > target) {
-        continue;
-      }
-      int mEnd = i + 1 > k ? k : i + 1;
-      for (int m = mEnd; m >= 1; m--) {
+    dp[0][0] = 1;
+    for (int i = 0; i < A.length; i++) {
+      if (A[i] > target) { continue; }
+      int mEnd = i + 1 >= k ? k : i + 1;
+      for (int m = mEnd; m > 0; m--) {
         for (int n = target; n >= A[i]; n--) {
           dp[m][n] += dp[m - 1][n - A[i]];
         }
       }
-      dp[1][A[i]]++; 
     }
     return dp[k][target];
   }
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
     LintCode089KSum one = new LintCode089KSum();
     int[] A = {1, 2, 3, 4};
     int k = 2;
