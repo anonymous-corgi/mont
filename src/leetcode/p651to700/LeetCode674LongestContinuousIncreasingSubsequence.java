8e157827c71d562d4e7a1675e92f5f02b95e9d88
Jingze Huang <huang.jingze@columbia.edu>
2018-10-07 19:00:16 -0700

    [Mod] LeetCode674LongestContinuousIncreasingSubsequence

---
+++ src/leetcode/p651to700/LeetCode674LongestContinuousIncreasingSubsequence.java
@@ -0,0 +1,29 @@
+package leetcode.p651to700;
+
+public class LeetCode674LongestContinuousIncreasingSubsequence {
+  
+  public int longestIncreasingContinuousSubsequence(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    int max = 1;
+    int count = 1;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] > nums[i - 1]) {
+        count++;
+        max = Math.max(max, count);
+      } else {
+        count = 1;
+      }
+    }
+    return max;
+  }
+
+  public static void main(String[] args) {
+    LeetCode674LongestContinuousIncreasingSubsequence one = 
+        new LeetCode674LongestContinuousIncreasingSubsequence();
+    int[] A = {5,2,3,4,6,8,8,8,4,5,6,4,3,2,1};
+    System.out.println(one.longestIncreasingContinuousSubsequence(A));
+  }
+
+}
