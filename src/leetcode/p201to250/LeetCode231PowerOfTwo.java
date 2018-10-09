192608423f9c15f6fcb53bab629d5f75b58fe1f8
Jingze HUANG <huang.jingze@columbia.edu>
2018-10-09 10:54:11 -0700

    [Mod] LeetCode231PowerOfTwo

--- src/leetcode/p201to250/LeetCode231PowerOfTwo.java
+++ src/leetcode/p201to250/LeetCode231PowerOfTwo.java
@@ -3,12 +3,7 @@ package leetcode.p201to250;
 public class LeetCode231PowerOfTwo {
   
   public boolean isPowerOfTwo(int n) {
-    return (n > 0 && (n & (n - 1)) == 0);
-  }
-  
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    return n > 0 && (n & (n - 1)) == 0;
   }
 
 }
