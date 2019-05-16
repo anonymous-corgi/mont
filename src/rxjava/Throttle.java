0cc0c5b544632c81bc379d772e8a4e9275f72dd8
Ray Huang <huang.jingze@columbia.edu>
2019-05-16 15:28:50 -0700

    [Mod] Rxjava/Throttle addes new test

--- src/rxjava/Throttle.java
+++ src/rxjava/Throttle.java
@@ -1,32 +1,31 @@
 package rxjava;
 
-import java.util.concurrent.TimeUnit;
-
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.TestObserver;
+import io.reactivex.subjects.PublishSubject;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Throttle {
 
     private static long initTime;
 
     private static long getCurrentTime() {
-        return System.currentTimeMillis() - initTime;
+        return (System.currentTimeMillis() - initTime) / 10;
     }
 
     public static void main(String[] args) {
-        throttleWithTimeout();
+        throttleLatest();
     }
 
     // Example: Every first click will act and start a timeout to block the following clicks.
     private static void throttleFirst() {
-        Observable.merge(Observable.intervalRange(0, 13, 0, 50, TimeUnit.MILLISECONDS),
-                Observable.intervalRange(0, 6, 0, 75, TimeUnit.MILLISECONDS))
-                .doOnSubscribe(disposable -> initTime = System.currentTimeMillis())
-                .doOnNext(count -> System.out.println(
-                        "Observable doOnNext() at Time:" + getCurrentTime() + ",\tCount:" + count
-                                + "\t Send ->"))
-                .throttleFirst(30, TimeUnit.MILLISECONDS)
+        createInterval()
+                .throttleFirst(40, TimeUnit.MILLISECONDS)
                 .blockingSubscribe(new Observer<Long>() {
                     @Override
                     public void onSubscribe(Disposable d) {
@@ -54,13 +53,8 @@ public class Throttle {
 
     // Example: Every click will start or restart a timer to do the action
     private static void throttleWithTimeout() {
-        Observable.merge(Observable.intervalRange(0, 13, 0, 50, TimeUnit.MILLISECONDS),
-                Observable.intervalRange(0, 6, 0, 75, TimeUnit.MILLISECONDS))
-                .doOnSubscribe(disposable -> initTime = System.currentTimeMillis())
-                .doOnNext(count -> System.out.println(
-                        "Observable doOnNext() at Time:" + getCurrentTime() + ",\tCount:" + count
-                                + "\t Send ->"))
-                .throttleWithTimeout(30, TimeUnit.MILLISECONDS)
+        createInterval()
+                .throttleWithTimeout(40, TimeUnit.MILLISECONDS)
                 .blockingSubscribe(new Observer<Long>() {
                     @Override
                     public void onSubscribe(Disposable d) {
@@ -88,13 +82,8 @@ public class Throttle {
 
     // throttleLast() pass down onNext() at fixed period.
     private static void throttleLast() {
-        Observable
-                .intervalRange(0, 13, 0, 50, TimeUnit.MILLISECONDS)
-                .doOnSubscribe(disposable -> initTime = System.currentTimeMillis())
-                .doOnNext(count -> System.out.println(
-                        "Observable doOnNext() at Time:" + getCurrentTime() + ",\tCount:" + count
-                                + "\t Send ->"))
-                .throttleLast(30, TimeUnit.MILLISECONDS)
+        createInterval()
+                .throttleLast(40, TimeUnit.MILLISECONDS)
                 .blockingSubscribe(new Observer<Long>() {
                     @Override
                     public void onSubscribe(Disposable d) {
@@ -118,5 +107,58 @@ public class Throttle {
                         System.out.println("Observer onComplete() at Time: " + getCurrentTime());
                     }
                 });
+    }
+
+    // throttleLatest() limits maximum that only one emission in a time span.
+    private static void throttleLatest() {
+        createInterval()
+                .throttleLatest(40, TimeUnit.MILLISECONDS)
+                .blockingSubscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Long count) {
+                        System.out.println(
+                                "Observer onComplete() at Time:" + getCurrentTime() + ",\tCount:" + count
+                                        + "\t Receive <-");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("Observer onComplete() at Time: " + getCurrentTime());
+                    }
+                });
+    }
+
+    @Test
+    public void createLocationDebouncePublisher() {
+        TestObserver<List<Long>> test = new TestObserver<>();
+        PublishSubject<Long> publisher = PublishSubject.create();
+
+        createInterval().subscribe(publisher);
+
+        publisher.buffer(publisher.debounce(500, TimeUnit.MILLISECONDS).take(1))
+                .take(2)
+                .blockingSubscribe(test);
+
+
+        test.assertComplete();
+    }
+
+    private static Observable<Long> createInterval() {
+        return Observable.merge(Observable.intervalRange(0, 7, 0, 60, TimeUnit.MILLISECONDS),
+                Observable.intervalRange(0, 4, 0, 90, TimeUnit.MILLISECONDS))
+                .doOnSubscribe(disposable -> initTime = System.currentTimeMillis())
+                .doOnNext(count -> System.out.println(
+                        "Observable doOnNext() at Time:" + getCurrentTime() + ",\tCount:" + count
+                                + "\t Send ->"));
     }
 }
