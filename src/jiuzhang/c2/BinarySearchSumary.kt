a351d440e4c0c20b86629ee4e33439509fb322b7
Ray Huang <huang.jingze@columbia.edu>
2019-06-04 07:53:30 -0700

    [Mod] BinarySearchSummary addes 2 methods

--- src/jiuzhang/c2/BinarySearchSumary.kt
+++ src/jiuzhang/c2/BinarySearchSumary.kt
@@ -30,6 +30,43 @@ class BinarySearchSumary {
         }
     }
 
+    // while (start < end)
+    class Classic2 : BinarySearch {
+
+        override fun search(nums: IntArray, target: Int): Int {
+            if (nums.isEmpty()) return -1
+            var start = 0
+            var end = nums.size - 1
+            while (start < end) {
+                val mid = start + (end - start) / 2
+                when {
+                    nums[mid] > target -> end = mid - 1
+                    nums[mid] < target -> start = mid + 1
+                    else -> return mid
+                }
+            }
+            return if (nums[start] == target) start else -1;
+        }
+    }
+
+    class Classic3 : BinarySearch {
+
+        override fun search(nums: IntArray, target: Int): Int {
+            if (nums.isEmpty()) return -1
+            var start = 0
+            var end = nums.size - 1
+            while (start < end) {
+                val mid = start + (end - start + 1) / 2
+                when {
+                    nums[mid] > target -> end = mid - 1
+                    nums[mid] < target -> start = mid + 1
+                    else -> return mid
+                }
+            }
+            return if (nums[end] == target) end else -1;
+        }
+    }
+
     class SearchFirstTarget : BinarySearch {
 
         override fun search(nums: IntArray, target: Int): Int {
