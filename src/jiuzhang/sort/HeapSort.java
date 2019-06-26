c7acd67fdc44c0f04e865007a9b04b0804a33c30
Ray Huang <huang.jingze@columbia.edu>
2019-06-25 23:11:11 -0700

    [Mod] Heap Sort

--- src/jiuzhang/sort/HeapSort.java
+++ src/jiuzhang/sort/HeapSort.java
@@ -1,58 +1,63 @@
 package jiuzhang.sort;
 
+import utils.Print;
+
 public class HeapSort {
-	
-	public void heapsort(int nums[])
-    {
+
+    public void heapsort(int[] nums) {
         int len = nums.length;
- 
-        // Build heap (rearrange array)
-        for (int i = len / 2 - 1; i >= 0; i--)
+
+        // Build a MaxHeap
+        for (int i = len / 2 - 1; i >= 0; i--) {
             heapify(nums, len, i);
- 
-        // One by one extract an element from heap
-        for (int i = len - 1; i >= 0; i--)
-        {
-            // Move current root to end
-            int temp = nums[0];
-            nums[0] = nums[i];
-            nums[i] = temp;
- 
-            // call max heapify on the reduced heap
+        }
+
+        // One by one extract the max number from heap, and rearrange the array.
+        for (int i = len - 1; i >= 0; i--) {
+            // Move the max number to end.
+            swap(nums, 0, i);
+
+            // Call Heapify function to rearrange the disordered number.
             heapify(nums, i, 0);
         }
     }
- 
-    // To heapify a subtree rooted with node i which is
-    // an index in arr[]. n is size of heap
-    void heapify(int nums[], int len, int index)
-    {
-        int maxIndex = index;  // Initialize largest as root
-        int left = 2 * index + 1;  // left = 2*i + 1
-        int right = 2 * index + 2;  // right = 2*i + 2
- 
+
+    // To heapify a subtree rooted with node i. n is size of heap
+    // Heapify function works only when node i is the only disordered node within this subtree.
+    void heapify(int[] nums, int len, int i) {
+        int maxIndex = i;  // Initialize largest as root
+        int lIndex = 2 * i + 1;  // left = 2 * i + 1
+        int rIndex = 2 * i + 2;  // right = 2 * i + 2
+
         // If left child is larger than root
-        if (left < len && nums[left] > nums[maxIndex])
-            maxIndex = left;
- 
+        if (lIndex < len && nums[lIndex] > nums[maxIndex]) {
+            maxIndex = lIndex;
+        }
+
         // If right child is larger than largest so far
-        if (right < len && nums[right] > nums[maxIndex])
-            maxIndex = right;
- 
+        if (rIndex < len && nums[rIndex] > nums[maxIndex]) {
+            maxIndex = rIndex;
+        }
+
         // If largest is not root
-        if (maxIndex != index) {
-            int swap = nums[index];
-            nums[index] = nums[maxIndex];
-            nums[maxIndex] = swap;
- 
+        if (maxIndex != i) {
+            swap(nums, i, maxIndex);
+
             // Recursively heapify the affected sub-tree
             heapify(nums, len, maxIndex);
         }
     }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    private void swap(int[] nums, int x, int y) {
+        int swap = nums[x];
+        nums[x] = nums[y];
+        nums[y] = swap;
+    }
 
-	}
-
+    public static void main(String[] args) {
+        HeapSort one = new HeapSort();
+        int[] array = new int[]{8, 6, 7, 5, 3, 1, 4, 2, 9};
+        one.heapsort(array);
+        Print.printArray(array);
+    }
 }
