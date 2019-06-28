664526fb0cb86ff9920d4b5e595533569f9f28b9
Ray Huang <huang.jingze@columbia.edu>
2019-06-27 23:19:47 -0700

    [Mod] GCD LCM

--- src/algorithm/GCD_LCM.java
+++ src/algorithm/GCD_LCM.java
@@ -1,42 +1,75 @@
 package algorithm;
 
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
 public class GCD_LCM {
-  
-  //Greatest Common Divisor
-  //It needs a wrapper method to handler corner cases like: a or b is 0;
-  public static int getGCD(int a, int b) {
-    return b == 0 ? a : getGCD(b, a % b);
-  }
-  
-  //Lowest Common Multiple
-  public static int getLCM(int a, int b) {
-    if (a == 0 || b == 0) {
-      return 0;
+
+    // Greatest Common Divisor
+    private interface GCD {
+        int getGCD(int a, int b);
     }
-    return  a * b / getGCD(a, b);
-  }
-  
-  public static int getGCDNonRecusive(int a, int b) {
-    if (a == 0 || b == 0) {
-      return 0;
+
+    // Lowest Common Multiple
+    private interface LCM {
+        int getLCM(int a, int b);
     }
-    do {
-      if (b > a) {
-        int temp = b;
-        b = a;
-        a = temp;
-      }
-      a = a % b;			
-    } while (a != 0);
-    return b;
-  }
-  
-  
-  public static void main(String[] args) {
-    int a = 0;
-    int b = 18;
-    System.out.println(getGCD(a, b));
-    System.out.println(getGCDNonRecusive(a, b));
-  }
-  
+
+    // It needs a wrapper method to handler corner cases like: a or b is 0;
+    private static class Recursive_GCD_Method implements GCD {
+
+        @Override
+        public int getGCD(int a, int b) {
+            return b == 0 ? a : getGCD(b, a % b);
+        }
+    }
+
+    private static class NonRecursive_GCD_Method implements GCD {
+
+        @Override
+        public int getGCD(int a, int b) {
+            while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+
+    private static class Recursive_LCM_Method implements LCM {
+
+        @Override
+        public int getLCM(int a, int b) {
+            if (a == 0 || b == 0) {
+                return 0;
+            }
+            return a * b / getGCDMethod().getGCD(a, b);
+        }
+    }
+
+    private static GCD getGCDMethod() {
+        return new Recursive_GCD_Method();
+    }
+
+    private static LCM getLCMMethod() {
+        return new Recursive_LCM_Method();
+    }
+
+    @Test
+    public void testGCD() {
+        GCD method1 = new Recursive_GCD_Method();
+        GCD method2 = new NonRecursive_GCD_Method();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int a = random.nextInt(Integer.MAX_VALUE);
+            int b = random.nextInt(Integer.MAX_VALUE);
+            int r1 = method1.getGCD(a, b);
+            int r2 = method2.getGCD(a, b);
+            assertEquals(r1, r2);
+        }
+    }
 }
