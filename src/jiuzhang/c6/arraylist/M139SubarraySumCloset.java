621916d0a607c938b273926c3cfa46885359f9b6
Ray Huang <huang.jingze@columbia.edu>
2019-06-29 19:21:11 -0700

    [Mod] M139SubarraySumCloset

--- src/jiuzhang/c6/arraylist/M139SubarraySumCloset.java
+++ src/jiuzhang/c6/arraylist/M139SubarraySumCloset.java
@@ -3,51 +3,59 @@ package jiuzhang.c6.arraylist;
 import java.util.Arrays;
 import java.util.Comparator;
 
+/**
+ * 139. Subarray Sum Closest
+ * Given an integer array, find a subarray with sum closest to zero. Return the indexes of the first number and last number.
+ * <p>
+ * Example
+ * Example1
+ * <p>
+ * Input:
+ * [-3,1,1,-3,5]
+ * Output:
+ * [0,2]
+ * Explanation: [0,2], [1,3], [1,1], [2,2], [0,4]
+ * Challenge
+ * O(nlogn) time
+ */
 public class M139SubarraySumCloset {
-	
+
     public int[] subarraySumClosest(int[] nums) {
-    	int[] result = new int[2];
-    	if(nums == null || nums.length ==0){
-    		return result;
-    	}
-    	Record one = new Record();
-        Record[] unit= new Record[nums.length + 1];
-        unit[0] = new Record(0, 0);
-        
-        for(int i = 0; i < nums.length + 1; i++){
-        	int sum = nums[i] + unit[i - 1].sum;
-            unit[i] = new Record(i, sum);
+        if (nums == null || nums.length == 0) {
+            return new int[2];
         }
-        
-        Arrays.sort(unit, one);
-        
+
+        int[] result = new int[2];
+        Record[] records = new Record[nums.length + 1];
+        records[0] = new Record(-1, 0);
+        for (int i = 1; i < records.length; i++) {
+            int sum = nums[i - 1] + records[i - 1].sum;
+            records[i] = new Record(i - 1, sum);
+        }
+
+        Arrays.sort(records, Comparator.comparingInt(a -> a.sum));
+
         int minSum = Integer.MAX_VALUE;
-        for(int i = 0; i < nums.length; i++){
-        	if(one.compare(unit[i + 1], unit[i]) < minSum){
-        		minSum = one.compare(unit[i + 1], unit[i]);
-        		int[] temp = new int[]{unit[i].index - 1, unit[i - 1].index - 1};
-                Arrays.sort(temp);
-        		result[0] = temp[0] + 1;
-        		result[1] = temp[1];
-        	}
+        for (int i = 0; i < nums.length; i++) {
+            int diff = records[i + 1].sum - records[i].sum;
+            if (diff < minSum) {
+                minSum = diff;
+                result[0] = records[i].index;
+                result[1] = records[i + 1].index;
+                Arrays.sort(result);
+                result[0]++;
+            }
         }
         return result;
     }
-    
-    private class Record implements Comparator<Record>{
-        public int index;
-        public int sum;
-        
-        public Record(){
-        }
-        public Record(int i, int s){
+
+    private class Record {
+        private int index;
+        private int sum;
+
+        Record(int i, int s) {
             index = i;
             sum = s;
         }
-        
-        public int compare(Record a, Record b){
-            return a.sum - b.sum;
-        }
     }
-
 }
