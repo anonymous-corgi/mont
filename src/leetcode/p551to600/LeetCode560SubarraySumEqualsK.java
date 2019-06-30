67dbef2209ab9e1e178bfb00bdb972844362ca1d
Ray Huang <huang.jingze@columbia.edu>
2019-06-29 20:00:19 -0700

    [Mod] LeetCode560SubarraySumEqualsK

--- src/leetcode/p551to600/LeetCode560SubarraySumEqualsK.java
+++ src/leetcode/p551to600/LeetCode560SubarraySumEqualsK.java
@@ -3,37 +3,34 @@ package leetcode.p551to600;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+ *
+ * Example 1:
+ * Input:nums = [1,1,1], k = 2
+ * Output: 2
+ * Note:
+ * The length of the array is in range [1, 20,000].
+ * The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+ */
 public class LeetCode560SubarraySumEqualsK {
-  
-  public int subarraySum(int[] nums, int k) {
-    if (nums == null || nums.length == 0) {
-      return 0;
-    }
-    int sum = 0;
-    int res = 0;
-    Map<Integer, Integer> map = new HashMap<>();
-    map.put(0, 1);
-    for (int i = 0; i < nums.length; i++) {
-      sum += nums[i];            
-      if (map.containsKey(sum - k)) {
-        res += map.get(sum - k);
-      }      
-      if (map.containsKey(sum)) {
-        map.put(sum, map.get(sum) + 1);
-      } else {
-        map.put(sum, 1);
-      }
-    }
-    return res;
-  }
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    LeetCode560SubarraySumEqualsK one = 
-        new LeetCode560SubarraySumEqualsK();
-    int[] nums = {-1,-1,1};
-    int k = 0;
-    System.out.print(one.subarraySum(nums, k));
-  }
-
+    public int subarraySum(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int sum = 0;
+        int res = 0;
+        Map<Integer, Integer> sums = new HashMap<>();
+        sums.put(0, 1);
+        for (int num : nums) {
+            sum += num;
+            if (sums.containsKey(sum - k)) {
+                res += sums.get(sum - k);
+            }
+            Integer count = sums.getOrDefault(sum, 0) + 1;
+            sums.put(sum, count);
+        }
+        return res;
+    }
 }
