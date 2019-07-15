cc2f21289c721a765bd3a6a34779f3f4370f84f0
Ray Huang <huang.jingze@columbia.edu>
2019-07-14 22:27:05 -0700

    [Mod] Formatted BackPack V-VII

--- src/jiuzhang/dp/backpack/BackPackV.java
+++ src/jiuzhang/dp/backpack/BackPackV.java
@@ -3,19 +3,26 @@ package jiuzhang.dp.backpack;
 public class BackPackV {
 
 	//Count How many ways can get to target without repeated use
-	public int backPackV(int[] weights, int capacity) {
-		if (weights == null || weights.length == 0) {
-			return 0;
-		}
-		int[] dp = new int[capacity + 1];
-		dp[0] = 1;
-		for (int i = 0; i < weights.length; i++) {
-			//If j starts from weights[i] might cause duplication
-			for (int j = capacity; j >= weights[i]; j--) {
-				dp[j] += dp[j - weights[i]];
-			}
-		}
-		return dp[capacity];
+	private interface Method {
+		int backPackV(int[] weights, int capacity);
 	}
 
+	private class Method1 implements Method {
+
+		@Override
+		public int backPackV(int[] weights, int capacity) {
+			if (weights == null || weights.length == 0) {
+				return 0;
+			}
+			int[] dp = new int[capacity + 1];
+			dp[0] = 1;
+			for (int weight : weights) {
+				//If j starts from weights[i] might cause duplication
+				for (int j = capacity; j >= weight; j--) {
+					dp[j] += dp[j - weight];
+				}
+			}
+			return dp[capacity];
+		}
+	}
 }
--- src/jiuzhang/dp/backpack/BackPackVI.java
+++ src/jiuzhang/dp/backpack/BackPackVI.java
@@ -3,9 +3,10 @@ package jiuzhang.dp.backpack;
 /**
  * LintCode 564. Combination Sum IV
  * LeetCode 377. Combination Sum IV Description Given an integer
+ * <p>
  * array nums with all positive numbers and no duplicates, find the number of possible combinations
  * that add up to a positive integer target.
- *
+ * <p>
  * Example Given nums = [1, 2, 4], target = 4 The possible combination ways are:
  * [1, 1, 1, 1]
  * [1, 1, 2]
@@ -17,22 +18,29 @@ package jiuzhang.dp.backpack;
  */
 public class BackPackVI {
 
-  //Count How many ways can get to target with repeated use
-  public int backPackVI(int[] weights, int capacity) {
-    if (weights == null || weights.length == 0) {
-      return 0;
+    //Count How many ways can get to target with repeated use. Permutation of coins matters.
+    private interface Method {
+        int backPackVI(int[] weights, int capacity);
     }
-    int[] dp = new int[capacity + 1];
-    dp[0] = 1;
-    for (int i = 1; i <= capacity; i++) {
-      //j starts from 0, because it contains duplication.
-      for (int j = 0; j < weights.length; j++) {
-        if (weights[j] <= i) {
-          dp[i] += dp[i - weights[j]];
-        }
-      }
-    }
-    return dp[capacity];
-  }
 
+    private class Method1 implements Method {
+
+        @Override
+        public int backPackVI(int[] weights, int capacity) {
+            if (weights == null || weights.length == 0) {
+                return 0;
+            }
+            int[] dp = new int[capacity + 1];
+            dp[0] = 1;
+            for (int i = 1; i <= capacity; i++) {
+                //j starts from 0, because it contains duplication.
+                for (int weight : weights) {
+                    if (weight <= i) {
+                        dp[i] += dp[i - weight];
+                    }
+                }
+            }
+            return dp[capacity];
+        }
+    }
 }
