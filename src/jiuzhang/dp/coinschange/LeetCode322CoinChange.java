8e481c7c9dcb79026384732a94ae33ec65b2019b
Ray Huang <huang.jingze@columbia.edu>
2019-07-14 22:52:03 -0700

    [New] LeetCode518CoinChange2

--- src/jiuzhang/dp/coinschange/LeetCode322CoinChange.java
+++ src/jiuzhang/dp/coinschange/LeetCode322CoinChange.java
@@ -1,27 +1,39 @@
 package jiuzhang.dp.coinschange;
 
+/**
+ * LeetCode 322. Coin Change
+ * <p>
+ * You are given coins of different denominations and a total amount of money amount.
+ * Write a function to compute the fewest number of coins that you need to make up that amount.
+ * If that amount of money cannot be made up by any combination of the coins, return -1.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: coins = [1, 2, 5], amount = 11
+ * Output: 3
+ * Explanation: 11 = 5 + 5 + 1
+ * Example 2:
+ * <p>
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ */
 public class LeetCode322CoinChange {
-	
-	public int coinChange(int[] coins, int amount) {
-		if (coins == null || coins.length == 0) {
-			return -1;
-		}
-		int[] dp = new int[amount + 1];
-		for (int i = 1; i <= amount; i++) {
-			dp[i] = -1;
-			for (int j = 0, cLen = coins.length; j < cLen; j++) {
-				if (i >= coins[j] && dp[i - coins[j]] != -1) {
-					if (dp[i] == -1 || dp[i - coins[j]] + 1 < dp[i]) {
-						dp[i] = dp[i - coins[j]] + 1;
-					}
-				}
-			}
-		}
-		return dp[amount];
-	}
 
-	public static void main(String[] args) {
-
-	}
-
+    public int coinChange(int[] coins, int amount) {
+        if (coins == null || coins.length == 0) {
+            return -1;
+        }
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = -1;
+            for (int coin : coins) {
+                if (i >= coin && dp[i - coin] != -1) {
+                    if (dp[i] == -1 || dp[i - coin] + 1 < dp[i]) {
+                        dp[i] = dp[i - coin] + 1;
+                    }
+                }
+            }
+        }
+        return dp[amount];
+    }
 }
