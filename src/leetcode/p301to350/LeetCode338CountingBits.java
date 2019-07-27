f893e19d9117157e38f18c3922c928e8b7136efb
Jingze Huang <huang.jingze@columbia.edu>
2019-07-27 12:14:20 -0700

    [Mod] LeetCode338CountingBits

--- src/leetcode/p301to350/LeetCode338CountingBits.java
+++ src/leetcode/p301to350/LeetCode338CountingBits.java
@@ -1,37 +1,32 @@
 package leetcode.p301to350;
 
-public class LeetCode338CountingBits {
-  
-  public static class MoveBit_method {
-    
-    public int[] countBits(int num) {
-      int[] dp = new int[num + 1];
-      for (int i = 0; i <= num; i++) {
-        dp[i] = dp[i >> 1] + (i & 1); 
-      }
-      return dp;
-    }
-    
-  }
-  
-  public static class BitOP_method {
-    
-    public int[] countBits(int num) {
-      int[] dp = new int[num + 1];
-      for (int i = 1; i <= num; i++) {
-        dp[i] = dp[i & (i - 1)] + 1;
-      }
-      return dp;
-    }
-    
-  }
+class LeetCode338CountingBits {
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    LeetCode338CountingBits.BitOP_method one = 
-        new LeetCode338CountingBits.BitOP_method();
-    int num = 10;
-    System.out.println(one.countBits(num));
-  }
+    private interface Method {
+        int[] countBits(int num);
+    }
 
+    private static class MoveBit_method implements Method {
+
+        @Override
+        public int[] countBits(int num) {
+            int[] dp = new int[num + 1];
+            for (int i = 0; i <= num; i++) {
+                dp[i] = dp[i >> 1] + (i & 1);
+            }
+            return dp;
+        }
+    }
+
+    private static class BitOP_method implements Method {
+
+        @Override
+        public int[] countBits(int num) {
+            int[] dp = new int[num + 1];
+            for (int i = 1; i <= num; i++) {
+                dp[i] = dp[i & (i - 1)] + 1;
+            }
+            return dp;
+        }
+    }
 }
