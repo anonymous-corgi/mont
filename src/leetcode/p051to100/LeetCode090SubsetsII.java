e74660ebd740e9b230d2822e6dcaf7c64238c5a5
Jingze Huang <huang.jingze@columbia.edu>
2019-08-01 09:16:23 -0700

    [Mod] Subsets

--- src/leetcode/p051to100/LeetCode090SubsetsII.java
+++ src/leetcode/p051to100/LeetCode090SubsetsII.java
@@ -5,25 +5,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LeetCode090SubsetsII {
-  
-  public List<List<Integer>> subsetsWithDup(int[] nums) {
-    Arrays.sort(nums);
-    List<List<Integer>> res = new ArrayList<>();
-    dfs(nums, 0, new ArrayList<Integer>(), res);
-    return res;
-  }
-  
-  private void dfs(int[] nums,  int pos, List<Integer> sub, List<List<Integer>> res) {
-    res.add(sub);
-    
-    for (int i = pos; i < nums.length; i++) {
-      if (i != pos && nums[i] == nums[i - 1]) {
-        continue;
-      }
-      List<Integer> nSub = new ArrayList<Integer>(sub);
-      nSub.add(nums[i]);
-      dfs(nums, i + 1, nSub, res);
-    }
-  }
 
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return results;
+        }
+        Arrays.sort(nums);
+        dfs(nums, 0, new ArrayList<>(), results);
+        return results;
+    }
+
+    private void dfs(int[] nums, int start, List<Integer> builder, List<List<Integer>> results) {
+        results.add(builder);
+        for (int i = start; i < nums.length; i++) {
+            if (i != start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            List<Integer> result = new ArrayList<>(builder);
+            result.add(nums[i]);
+            dfs(nums, i + 1, result, results);
+        }
+    }
 }
