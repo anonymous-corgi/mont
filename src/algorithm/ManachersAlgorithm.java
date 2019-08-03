e4b1a2226a50567ff4b896345ba1a83229c77d16
Ray Huang <huang.jingze@columbia.edu>
2019-08-03 00:28:16 -0700

    [Mod] ManachersAlgorithm

--- src/algorithm/ManachersAlgorithm.java
+++ src/algorithm/ManachersAlgorithm.java
@@ -1,92 +1,90 @@
 package algorithm;
 
+/**
+ * @see leetcode.p001to050.LeetCode005LongestPalindromicSubstring
+ */
 public class ManachersAlgorithm {
 
-  private static final char SEPARATOR = '*';
-  
-  public String longestPalindrome(String s) {
-    if (s == null || s.length() == 0) {
-      return "";
-    }
-    
-    char[] chs = addBoundaries(s.toCharArray());
-    int[] p = new int[chs.length];
-    // c represents the center of the current Palindrome.
-    // r represents the right radius of the current Palindrome.
-    int c = 0, r = 0;
-    // f: front, b: back. The walking indexes to compare if two elements are the same.
-    int f = 0, b = 0; 
-    for (int i = 0; i < chs.length; i++) {
-      if (i > r) {
-        b = i + 1;
-        f = i - 1;
-      } else {
-        // im the mirror index of i to c.
-        int im = c * 2 - i;
-        if (p[im] + i < r) {
-          p[i] = p[im];
-          f = -1; // This signals bypassing the while loop below. 
-        } else {
-          p[i] = r - i;
-          b = r + 1;
-          f = i * 2 - b;
+    private static final char SEPARATOR = '*';
+
+    public static String longestPalindrome(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
         }
-      }
-      
-      while (f >= 0 && b < chs.length && chs[f] == chs[b]) {
-        p[i]++;
-        f--;
-        b++;
-      }
-      
-      if ((i + p[i]) > r) {
-        c = i;
-        r = i + p[i];
-      }
+
+        char[] chs = addBoundaries(s.toCharArray());
+        int[] p = new int[chs.length];
+        // c is the center of the current Palindrome.
+        // m is the right margin of the current Palindrome.
+        int c = 0, m = 0;
+        // l: left, r: right. The two walking indexes to compare if the two elements are the same.
+        int l = 0, r = 0;
+        for (int i = 0; i < chs.length; i++) {
+            // i >= m indicates that we can't use the p[im] to simplify calculation.
+            if (i >= m) {
+                r = i + 1;
+                l = i - 1;
+            } else {
+                // im is the mirror index of i as to c.
+                int im = c * 2 - i;
+                if (i + p[im] >= m) {
+                    p[i] = m - i;
+                    r = m + 1;
+                    l = i * 2 - r;
+                } else {
+                    p[i] = p[im];
+                    l = -1; // This signal bypasses the while loop below.
+                }
+            }
+
+            while (l >= 0 && r < chs.length && chs[l] == chs[r]) {
+                p[i]++;
+                r++;
+                l--;
+            }
+
+            // Update m
+            if ((i + p[i]) > m) {
+                c = i;
+                m = i + p[i];
+            }
+        }
+
+        // Search the longest Palindrome.
+        int maxC = 0, maxLen = 0;
+        for (int i = 1; i < chs.length; i++) {
+            if (p[i] > maxLen) {
+                maxC = i;
+                maxLen = p[i];
+            }
+        }
+
+        return removeBoundaries(chs, maxC - maxLen, maxC + maxLen + 1);
     }
-    
-    // Search the longest Palindrome.
-    int maxC = 0, maxLen = 0;
-    for (int i = 1; i < chs.length; i++) {
-      if (p[i] > maxLen) {
-        maxC = i;
-        maxLen = p[i];
-      }
+
+    private static char[] addBoundaries(char[] cs) {
+        if (cs == null || cs.length == 0) {
+            return new char[]{SEPARATOR};
+        }
+
+        char[] chs = new char[cs.length * 2 + 1];
+        for (int i = 0; i < (chs.length - 1); i = i + 2) {
+            chs[i] = SEPARATOR;
+            chs[i + 1] = cs[i / 2];
+        }
+        chs[chs.length - 1] = SEPARATOR;
+        return chs;
     }
-    
-    return removeBoundaries(chs, maxC - maxLen, maxC + maxLen + 1);
-  }
-  
-  private char[] addBoundaries(char[] cs) {
-    if (cs == null || cs.length == 0) {
-      return "|".toCharArray();        	
+
+    private static String removeBoundaries(char[] cs, int start, int end) {
+        if (cs == null || cs.length < 3) {
+            return "";
+        }
+
+        char[] res = new char[(end - start) / 2];
+        for (int i = start + 1, j = 0; i < end; i += 2, j++) {
+            res[j] = cs[i];
+        }
+        return String.valueOf(res);
     }
-    
-    char[] chs = new char[cs.length * 2 + 1];
-    for (int i = 0; i < (chs.length - 1); i = i + 2) {
-      chs[i] = SEPARATOR;
-      chs[i + 1] = cs[i / 2];
-    }
-    chs[chs.length - 1] = SEPARATOR;
-    return chs;
-  }
-  
-  private String removeBoundaries(char[] cs, int start, int end) {
-    if (cs == null || cs.length < 3) {
-      return "";
-    }
-    
-    char[] res = new char[(end - start) / 2];
-    for (int i = start + 1, j = 0; i < end; i += 2, j++) {
-      res[j] = cs[i];
-    }
-    return String.valueOf(res);
-  }
-  
-  public static void main(String[] args) {
-    String s =  "forgeeksskeegfor";
-    ManachersAlgorithm one = new ManachersAlgorithm();
-    System.out.println(one.longestPalindrome(s));
-  }
-  
 }
