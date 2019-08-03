08ceb10eb4460df0de30309b7673c0d26e9d4b4a
Jingze Huang <huang.jingze@columbia.edu>
2019-08-03 14:34:03 -0700

    [Mod] Coins in a line

---
+++ src/lintcode/p351to400/LintCode394CoinsInALineI.java
@@ -0,0 +1,60 @@
+package lintcode.p351to400;
+
+/**
+ * LintCode 394. Coins in a Line I
+ * Medium
+ * <p>
+ * There are n coins in a line.
+ * Two players take turns to take one or two coins from right side until there are no more coins left.
+ * The player who take the last coin wins.
+ * <p>
+ * Could you please decide the first player will win or lose?
+ * <p>
+ * If the first player wins, return true, otherwise return false.
+ * Have you met this question in a real interview?  Yes
+ * Problem Correction
+ * <p>
+ * Example
+ * Example 1:
+ * Input: 1
+ * Output: true
+ * <p>
+ * Example 2:
+ * Input: 4
+ * Output: true
+ * <p>
+ * Explanation:
+ * The first player takes 1 coin at first. Then there are 3 coins left.
+ * Whether the second player takes 1 coin or two, then the first player can take all coin(s) left.
+ */
+public class LintCode394CoinsInALineI {
+
+    private interface Method {
+        boolean firstWillWin(int n);
+    }
+
+    private static class Cheating_Method implements Method {
+
+        @Override
+        public boolean firstWillWin(int n) {
+            return n % 3 != 0;
+        }
+    }
+
+    private static class DP_Method implements Method {
+
+        @Override
+        public boolean firstWillWin(int n) {
+            if (n < 3) {
+                return n != 0;
+            }
+            boolean[] dp = new boolean[n];
+            dp[0] = true;
+            dp[1] = true;
+            for (int i = 2; i < n; i++) {
+                dp[i] = !(dp[i - 1] && dp[i - 2]);
+            }
+            return dp[n - 1];
+        }
+    }
+}
---
+++ src/lintcode/p351to400/LintCode395CoinsInALineII.java
@@ -0,0 +1,41 @@
+package lintcode.p351to400;
+
+/**
+ * LintCode 395. Coins in a Line II
+ * <p>
+ * There are n coins with different value in a line.
+ * Two players take turns to take one or two coins from left side until there are no more coins left.
+ * The player who take the coins with the most value wins.
+ * <p>
+ * Could you please decide the first player will win or lose?
+ * <p>
+ * If the first player wins, return true, otherwise return false.
+ * <p>
+ * Have you met this question in a real interview?
+ * <p>
+ * Example
+ * Example 1:
+ * Input: [1, 2, 2]
+ * Output: true
+ * Explanation: The first player takes 2 coins.
+ * <p>
+ * Example 2:
+ * Input: [1, 2, 4]
+ * Output: false
+ * Explanation: Whether the first player takes 1 coin or 2, the second player will gain more value.
+ */
+public class LintCode395CoinsInALineII {
+
+    public boolean firstWillWin(int[] values) {
+        if (values.length < 3) {
+            return values.length != 0;
+        }
+        int[] dp = new int[values.length];
+        dp[dp.length - 1] = values[dp.length - 1];
+        dp[dp.length - 2] = values[dp.length - 2] + values[dp.length - 1];
+        for (int i = dp.length - 3; i >= 0; i--) {
+            dp[i] = Math.max(values[i] - dp[i + 1], values[i] + values[i + 1] - dp[i + 2]);
+        }
+        return dp[0] > 0;
+    }
+}
