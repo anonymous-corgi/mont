0afea819446148987c93c181dfb01668a8b8c81c
Ray Huang <huang.jingze@columbia.edu>
2019-08-12 23:11:04 -0700

    [Mod] LeetCode101SymmetricTree

--- src/leetcode/p101to150/LeetCode101SymmetricTree.java
+++ src/leetcode/p101to150/LeetCode101SymmetricTree.java
@@ -3,18 +3,29 @@ package leetcode.p101to150;
 import basicclass.TreeNode;
 
 public class LeetCode101SymmetricTree {
-  
-  public boolean isSymmetric(TreeNode root) {
-    if (root == null) { return true; }
-    return isSymmetric(root.left, root.right);
-  }
-  
-  private boolean isSymmetric(TreeNode l, TreeNode r) {
-    if (l == null && r == null) { return true; }
-    if (l == null || r == null || l.val != r.val) {
-      return false;
+
+    private interface Method {
+        boolean isSymmetric(TreeNode root);
     }
-    return isSymmetric(l.left, r.right) && isSymmetric(l.right, r.left);
-  }
-  
+
+    private static final class DFS implements Method {
+
+        @Override
+        public boolean isSymmetric(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            return isSymmetric(root.left, root.right);
+        }
+
+        private boolean isSymmetric(TreeNode l, TreeNode r) {
+            if (l == null && r == null) {
+                return true;
+            }
+            if (l == null || r == null || l.val != r.val) {
+                return false;
+            }
+            return isSymmetric(l.left, r.right) && isSymmetric(l.right, r.left);
+        }
+    }
 }
