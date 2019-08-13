2a464b53eb4253bd326e6a8575201ea82e34c3f8
Ray Huang <huang.jingze@columbia.edu>
2019-08-12 23:44:14 -0700

    [Mod] LeetCode102BinaryTreeLevelOrderTraversal

--- src/leetcode/p101to150/LeetCode102BinaryTreeLevelOrderTraversal.java
+++ src/leetcode/p101to150/LeetCode102BinaryTreeLevelOrderTraversal.java
@@ -1,68 +1,68 @@
 package leetcode.p101to150;
 
-import basicclass.TreeNode;
 import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Queue;
+
+import basicclass.TreeNode;
 
 public class LeetCode102BinaryTreeLevelOrderTraversal {
 
-  interface Method {
-    List<List<Integer>> levelOrder(TreeNode root);
-  }
+    private interface Method {
+        List<List<Integer>> levelOrder(TreeNode root);
+    }
 
-  public static class BFS_method implements Method {
+    private static class BFS implements Method {
 
-    @Override
-    public List<List<Integer>> levelOrder(TreeNode root) {
-      List<List<Integer>> res = new ArrayList<>();
-      if (root == null) {
-        return res;
-      }
-      Queue<TreeNode> taskList = new ArrayDeque<>();
-      taskList.offer(root);
-      while (!taskList.isEmpty()) {
-        List<Integer> levelRes = new ArrayList<>();
-        for (int i = 0, iSize = taskList.size(); i < iSize; i++) {
-          TreeNode cursor = taskList.poll();
-          levelRes.add(cursor.val);
-          if (cursor.left != null) {
-            taskList.offer(cursor.left);
-          }
-          if (cursor.right != null) {
-            taskList.offer(cursor.right);
-          }
+        @Override
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            if (root == null) {
+                return Collections.emptyList();
+            }
+            List<List<Integer>> res = new ArrayList<>();
+            Queue<TreeNode> taskQueue = new ArrayDeque<>();
+            taskQueue.offer(root);
+            while (!taskQueue.isEmpty()) {
+                int len = taskQueue.size();
+                List<Integer> level = new ArrayList<>(len);
+                for (int i = 0; i < len; i++) {
+                    TreeNode cursor = taskQueue.poll();
+                    level.add(cursor.val);
+                    if (cursor.left != null) {
+                        taskQueue.offer(cursor.left);
+                    }
+                    if (cursor.right != null) {
+                        taskQueue.offer(cursor.right);
+                    }
+                }
+                res.add(level);
+            }
+            return res;
         }
-        res.add(levelRes);
-      }
-      return res;
     }
 
-  }
+    public static class Recursive implements Method {
 
-  public static class Recursive_method implements Method {
+        @Override
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            return levelOrder(root, 0, new ArrayList<>());
+        }
 
-    @Override
-    public List<List<Integer>> levelOrder(TreeNode root) {
-      return levelOrder(root, 0, new ArrayList<>());
+        private List<List<Integer>> levelOrder(TreeNode root, int depth, List<List<Integer>> res) {
+            if (root == null) {
+                return res;
+            }
+            if (depth >= res.size()) {
+                res.add(new ArrayList<>());
+            }
+            res.get(depth).add(root.val);
+
+            levelOrder(root.left, depth + 1, res);
+            levelOrder(root.right, depth + 1, res);
+
+            return res;
+        }
     }
-
-    private List<List<Integer>> levelOrder(TreeNode root, int depth, List<List<Integer>> list) {
-      if (root == null) {
-        return list;
-      }
-      if (depth >= list.size()) {
-        list.add(new ArrayList<Integer>());
-      }
-      list.get(depth).add(root.val);
-
-      levelOrder(root.left, depth + 1, list);
-      levelOrder(root.right, depth + 1, list);
-
-      return list;
-    }
-
-  }
-
 }
