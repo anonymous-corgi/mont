8092caa4566fb48234c5c889d1caff1b3995071a
Ray Huang <huang.jingze@columbia.edu>
2019-08-14 21:31:32 -0700

    [Mod] LeetCode238ProductOfArrayExceptSelf

--- src/leetcode/p201to250/LeetCode238ProductOfArrayExceptSelf.java
+++ src/leetcode/p201to250/LeetCode238ProductOfArrayExceptSelf.java
@@ -1,32 +1,21 @@
 package leetcode.p201to250;
 
-import utils.Print;
-//Google
 public class LeetCode238ProductOfArrayExceptSelf {
-  
-  public int[] productExceptSelf(int[] nums) {
-    if (nums == null || nums.length == 0) {
-      return new int[0];
-    }
-    int[] res = new int[nums.length];
-    res[0] = 1;
-    for (int i = 1; i < nums.length; i++) {
-      res[i] = res[i - 1] * nums[i - 1];
-    }
-    int right = 1;
-    for (int i = nums.length - 2; i >= 0; i--) {
-      right *= nums[i + 1];
-      res[i] *= right;
-    }
-    return res;
-  }
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    LeetCode238ProductOfArrayExceptSelf one = 
-        new LeetCode238ProductOfArrayExceptSelf();
-    int[] nums = {1, 2, 3, 4};
-    Print.printArray(one.productExceptSelf(nums));
-  }
-
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            right *= nums[i + 1];
+            result[i] *= right;
+        }
+        return result;
+    }
 }
