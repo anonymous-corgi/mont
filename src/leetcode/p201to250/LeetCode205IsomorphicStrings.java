55fe9831b90846b03273d256d296652873fa7c52
Ray Huang <huang.jingze@columbia.edu>
2019-08-18 11:39:27 -0700

    [Mod] LeetCode205IsomorphicStrings

--- src/leetcode/p201to250/LeetCode205IsomorphicStrings.java
+++ src/leetcode/p201to250/LeetCode205IsomorphicStrings.java
@@ -1,24 +1,67 @@
 package leetcode.p201to250;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class LeetCode205IsomorphicStrings {
-  
-  public boolean isIsomorphic(String s1, String s2) {
-    int[] m = new int[512];
-    for (int i = 0; i < s1.length(); i++) {
-      if (m[s1.charAt(i)] != m[s2.charAt(i) + 256]) {
-        return false;
-      }
-      m[s1.charAt(i)] = m[s2.charAt(i) + 256] = i + 1;
+
+    private interface Method {
+        boolean isIsomorphic(String s, String t);
     }
-    return true;
-  }
 
-  public static void main(String[] args) {
-    String s1 = "egg";
-    String s2 = "add";
-    LeetCode205IsomorphicStrings one =
-        new LeetCode205IsomorphicStrings();
-    System.out.println(one.isIsomorphic(s1, s2));
-  }
+    private static class PositionMap implements Method {
 
+        @Override
+        public boolean isIsomorphic(String s, String t) {
+            int[] m = new int[256];
+            for (int i = 0; i < s.length(); i++) {
+                if (m[s.charAt(i)] != m[t.charAt(i) + 128]) {
+                    return false;
+                }
+                m[s.charAt(i)] = m[t.charAt(i) + 128] = i + 1;
+            }
+            return true;
+        }
+    }
+
+    private static class TwoWayMap implements Method {
+
+        @Override
+        public boolean isIsomorphic(String s, String t) {
+            char[] st = new char[128];
+            char[] ts = new char[128];
+            for (int i = 0, iLen = s.length(); i < iLen; i++) {
+                char charS = s.charAt(i);
+                char charT = t.charAt(i);
+                if (st[charS] == 0 && ts[charT] == 0) {
+                    st[charS] = charT;
+                    ts[charT] = charS;
+                } else if (st[charS] != charT || ts[charT] != charS) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    private static Method getMethod() {
+        return new PositionMap();
+    }
+
+    @Test
+    public void testcase1() {
+        test("egg", "add", true);
+    }
+
+    @Test
+    public void testcase2() {
+        test("ab", "aa", false);
+    }
+
+    private void test(String s, String t, boolean expect) {
+        Method method = getMethod();
+        Assert.assertThat(method.isIsomorphic(s, t), is(expect));
+    }
 }
--- src/subjects/String.md
+++ src/subjects/String.md
@@ -1,9 +1,13 @@
 # String:
+
 ### 1. Palindrome:
 
 ### 2. Anagram:
 
 ### 3. Strobogrammatic:
-##### 1. [LeetCode 246. Strobogrammatic Number I ](/src/leetcode/p201to250/LeetCode246StrobogrammaticNumberI.java)
-##### 2. [LeetCode 247. Strobogrammatic Number II ](/src/leetcode/p201to250/LeetCode247StrobogrammaticNumberII.java)
-##### 3. [*LeetCode 248. Strobogrammatic Number III ](/src/leetcode/p201to250/LeetCode248StrobogrammaticNumberIII.java)
+##### 1. [LeetCode 246. Strobogrammatic Number I](/src/leetcode/p201to250/LeetCode246StrobogrammaticNumberI.java)
+##### 2. [LeetCode 247. Strobogrammatic Number II](/src/leetcode/p201to250/LeetCode247StrobogrammaticNumberII.java)
+##### 3. [*LeetCode 248. Strobogrammatic Number III](/src/leetcode/p201to250/LeetCode248StrobogrammaticNumberIII.java)
+
+### 4. Isomorphic:
+##### 1. [LeetCode 205. Isomorphic Strings](/src/leetcode/p201to250/LeetCode205IsomorphicStrings.java)
