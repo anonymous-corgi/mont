656856ab7c4295fab2bf11c20a77e34d5c75622b
Ray Huang <huang.jingze@columbia.edu>
2019-08-18 23:31:00 -0700

    [Mod] LeetCode152MaximumProductSubarray

--- src/jiuzhang/dp/LeetCode152MaximumProductSubarray.java
+++ src/jiuzhang/dp/LeetCode152MaximumProductSubarray.java
@@ -1,37 +1,57 @@
 package jiuzhang.dp;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class LeetCode152MaximumProductSubarray {
-  
-  public int maxProduct(int[] nums) {
-    if (nums == null || nums.length == 0) {
-      return 0;
-    }
-    int nLen = nums.length;
-    int max = nums[0];
-    int min = nums[0];
-    int result = nums[0];
-    for (int i = 1; i < nLen; i++) {
-      if (nums[i] > 0) {
-        max = Math.max(nums[i], max * nums[i]);
-        min = Math.min(nums[i], min * nums[i]);
-      } else if (nums[i] < 0) {
-        int preMax = max;
-        max = Math.max(nums[i], min * nums[i]);
-        min = Math.min(nums[i], preMax * nums[i]);
-      } else {
-        max = 0;
-        min = 0;
-      }
-      result = Math.max(result, max);
-    }
-    return result;
-  }
 
-  public static void main(String[] args) {
-    LeetCode152MaximumProductSubarray one
-    = new LeetCode152MaximumProductSubarray();
-    int[] nums = {-4,-3,-2};
-    System.out.println(one.maxProduct(nums));
-  }
+    private interface Method {
+        int maxProduct(int[] nums);
+    }
 
+    private static class Normal implements Method {
+
+        @Override
+        public int maxProduct(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            int max = nums[0];
+            int min = nums[0];
+            int result = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < 0) {
+                    int temp = max;
+                    max = min;
+                    min = temp;
+                }
+                max = Math.max(nums[i], nums[i] * max);
+                min = Math.min(nums[i], nums[i] * min);
+                result = Math.max(result, max);
+            }
+            return result;
+        }
+    }
+
+    private static Method getMethod() {
+        return new Normal();
+    }
+
+    @Test
+    public void testcase1() {
+        int[] nums = {2, 3, -2, 4};
+        test(nums, 6);
+    }
+
+    @Test
+    public void testcase2() {
+        int[] nums = {-4};
+        test(nums, -4);
+    }
+
+    private void test(int[] nums, int expect) {
+        assertThat(getMethod().maxProduct(nums), is(expect));
+    }
 }
