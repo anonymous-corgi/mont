b5e10e56369d8e88acc4e9482907683ba1a79364
Ray Huang <huang.jingze@columbia.edu>
2019-08-28 23:08:32 -0700

    [Mod] LeetCode060PermutationSequence

--- src/leetcode/p051to100/LeetCode060PermutationSequence.java
+++ src/leetcode/p051to100/LeetCode060PermutationSequence.java
@@ -3,33 +3,30 @@ package leetcode.p051to100;
 import java.util.LinkedList;
 
 public class LeetCode060PermutationSequence {
-  
-  public String getPermutation(int n, int k) {
-    if (n < 1 || k < 1) {
-      return "";
-    }
-    
-    int pos = 1;
-    LinkedList<Integer> source = new LinkedList<>();
-    StringBuilder sb = new StringBuilder();
-    for (int i = 1; i <= n; i++) {
-      pos *= i;
-      source.add(i);
-    }
-    k--;
-    int digitIndex = n;
-    while (digitIndex > 0) {
-      pos /= digitIndex--;
-      int i = k / pos;
-      k %= pos;
-      sb.append(source.remove(i));
-    }
-    return sb.toString();
-  }
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    private interface Method {
+        String getPermutation(int n, int k);
+    }
 
-  }
+    private static class NonRecursive implements Method {
 
+        @Override
+        public String getPermutation(int n, int k) {
+            int pos = 1;
+            StringBuilder sb = new StringBuilder();
+            LinkedList<Integer> source = new LinkedList<>();
+            for (int i = 1; i <= n; i++) {
+                pos *= i;
+                source.add(i);
+            }
+            k--;
+            for (int digitIndex = n; digitIndex > 0; digitIndex--) {
+                pos /= digitIndex;
+                int i = k / pos;
+                k %= pos;
+                sb.append(source.remove(i));
+            }
+            return sb.toString();
+        }
+    }
 }
