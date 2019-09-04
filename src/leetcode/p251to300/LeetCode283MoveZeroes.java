09130a638e9abdb53c9cee12ebfe493da0751c88
Ray Huang <huang.jingze@columbia.edu>
2019-09-03 18:17:37 -0700

    [Mod] LeetCode283MoveZeroes

--- src/leetcode/p251to300/LeetCode283MoveZeroes.java
+++ src/leetcode/p251to300/LeetCode283MoveZeroes.java
@@ -1,26 +1,26 @@
 package leetcode.p251to300;
 
 public class LeetCode283MoveZeroes {
-  
-  public void moveZeroes(int[] nums) {
-    if (nums == null || nums.length == 0) {
-      return;
+
+    private interface Method {
+        void moveZeroes(int[] nums);
     }
-    int left = -1;
-    int cursor = 0;
-    while (cursor < nums.length) {
-      if (nums[cursor] != 0) {
-        int temp = nums[++left];
-        nums[left] = nums[cursor];
-        nums[cursor] = temp;
-      }
-      cursor++;
+
+    private static class Normal implements Method {
+
+        @Override
+        public void moveZeroes(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return;
+            }
+            int left = -1;
+            for (int cursor = 0; cursor < nums.length; cursor++) {
+                if (nums[cursor] != 0) {
+                    int temp = nums[++left];
+                    nums[left] = nums[cursor];
+                    nums[cursor] = temp;
+                }
+            }
+        }
     }
-  }
-
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
-  }
-
 }
