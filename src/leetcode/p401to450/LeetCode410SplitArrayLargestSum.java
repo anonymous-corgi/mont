bde8e2a0855cd4533b01ea10759387af8b71792c
Ray Huang <huang.jingze@columbia.edu>
2019-09-05 09:40:21 -0700

    [Mod] LeetCode410SplitArrayLargestSum

--- src/leetcode/p401to450/LeetCode410SplitArrayLargestSum.java
+++ src/leetcode/p401to450/LeetCode410SplitArrayLargestSum.java
@@ -1,11 +1,12 @@
 package leetcode.p401to450;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
- * LeetCode 410. Split Array Largest Sum
+ * LeetCode 410. Split Array Largest Sum - Identical with: LintCode 437. Copy Book
  * Hard
  * <p>
  * Given an array which consists of non-negative integers and an integer m,
@@ -31,34 +32,109 @@ import org.junit.Test;
  * The best way is to split it into [7,2,5] and [10,8],
  * where the largest sum among the two subarrays is only 18.
  */
+@SuppressWarnings("unused")
 public class LeetCode410SplitArrayLargestSum {
 
     private interface Method {
         int splitArray(int[] nums, int m);
     }
 
-    private static class BinarySearch_Method implements Method {
+    private static abstract class DP implements Method {
 
         @Override
         public int splitArray(int[] nums, int m) {
             if (nums == null || nums.length == 0) {
                 return 0;
             }
-            long left = 0;
-            long right = 0;
-            for (int num : nums) {
-                left = Math.max(left, num);
-                right += num;
+            long[] dp = new long[nums.length + 1];
+            long[] prefix = new long[nums.length + 1];
+            for (int i = 1; i < dp.length; i++) {
+                prefix[i] = prefix[i - 1] + nums[i - 1];
+                dp[i] = prefix[i];
             }
-            while (left < right) {
-                long mid = (right + left) / 2;
-                if (canSplit(nums, mid, m)) {
-                    right = mid;
-                } else {
-                    left = mid + 1;
+
+            //  dp[i, s] means The optimal solution for the first i numbers that are divided into s groups.
+            //  Updating dp[i, s] to dp[i, s + 1], we need to let the last group
+            //  to include numbers from j + 1 to i,
+            //  while the s groups divides the first j numbers (dp[j, s])
+            //  And try to find a j making Max(dp[j, s], S[i] - S[j]) smallest
+            //  => dp[i, s + 1] = Min( Max(dp[j, s], S[i] - S[j]) ),  s <= j < i
+            for (int s = 2; s <= m; s++) {
+                //  i indexes the length of subArray
+                for (int pos = dp.length - 1; pos >= s; pos--) {
+                    dp[pos] = findMin(prefix, dp, s, pos);
                 }
             }
-            return (int) right;
+            return (int) dp[nums.length];
+        }
+
+        // Find a partition which makes the maximum of the two parts minimum.
+        abstract int findMin(long[] prefix, long[] dp, int share, int total);
+    }
+
+    private static class DP_Binary extends DP {
+
+        @Override
+        int findMin(long[] prefix, long[] dp, int share, int total) {
+            long min = Integer.MAX_VALUE;
+            int start = share - 1;
+            int end = total - 1;
+            while (start < end) {
+                int mid = start + (end - start) / 2;
+                long lastShare = prefix[total] - prefix[mid];
+                long diff = dp[mid] - lastShare;
+                if (diff < 0) {
+                    start = mid + 1;
+                } else if (diff > 0) {
+                    end = mid - 1;
+                } else {
+                    return (int) lastShare;
+                }
+                min = Math.min(min, Math.max(dp[mid], lastShare));
+            }
+            return (int) Math.min(min, Math.max(dp[start], prefix[total] - prefix[start]));
+        }
+    }
+
+    private static class DP_Pro extends DP {
+
+        @Override
+        int findMin(long[] prefix, long[] dp, int share, int total) {
+            //  prefix[i] - prefix[j] means the sum for the s-th part.
+            long min = Integer.MAX_VALUE;
+            for (int j = total - 1; j >= share - 1; j--) {
+                long max = Math.max(dp[j], prefix[total] - prefix[j]);
+                if (max > min) {
+                    break;
+                }
+                min = max;
+            }
+            return (int) min;
+        }
+    }
+
+    private static class BinarySearch implements Method {
+
+        @Override
+        public int splitArray(int[] nums, int m) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            long start = 0;
+            long end = 0;
+            for (int num : nums) {
+                start = Math.max(start, num);
+                end += num;
+            }
+            while (start < end) {
+                long mid = (end + start) / 2;
+                if (canSplit(nums, mid, m)) {
+                    end = mid;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            return (int) start;
         }
 
         private boolean canSplit(int[] nums, long maxSum, int maxGroups) {
@@ -79,67 +155,34 @@ public class LeetCode410SplitArrayLargestSum {
 
     }
 
-    private static class DP_Method implements Method {
-        //  This method reverse the update way as to the previous one,
-        //  making it much more similar to the DP solution given by Jiuzhang
-        @Override
-        public int splitArray(int[] nums, int m) {
-            long[] dp = new long[nums.length + 1];
-            long[] prefix = new long[nums.length + 1];
-            for (int i = 1; i < dp.length; i++) {
-                prefix[i] = prefix[i - 1] + nums[i - 1];
-                dp[i] = prefix[i];
-            }
-            //  dp[i, s] means The optimal solution for the first i numbers that are divided into s groups.
-            //  Updating dp[i, s] to dp[i, s + 1], we need to let the last group
-            //  to include numbers from j + 1 to i,
-            //  while the s groups divides the first j numbers (dp[j, s])
-            //  And try to find a j making Max(dp[j, s], S[i] - S[j]) smallest
-            //  => dp[i, s + 1] = Min( Max(dp[j, s], S[i] - S[j]) ),  s <= j < i
-            for (int s = 1; s < m; s++) {
-                //  i indexes the length of subArray
-                for (int i = dp.length - 1; i > s; i--) {
-                    //  prefix[i] - prefix[j] means the sum for the s-th part.
-                    for (int j = i - 1; j >= s; j--) {
-                        long max = Math.max(dp[j], prefix[i] - prefix[j]);
-                        if (max <= dp[i]) {
-                            dp[i] = max;
-                        } else {
-                            break;
-                        }
-                    }
-                }
-            }
-            return (int) dp[dp.length - 1];
-        }
-    }
-
     private static Method getMethod() {
-        int num = 1;
-        switch (num) {
-            case 0:
-                return new BinarySearch_Method();
-            default:
-                return new DP_Method();
-        }
-    }
-
-    private int splitArray(int[] nums, int m) {
-        return getMethod().splitArray(nums, m);
+        return new DP_Binary();
     }
 
     @Test
-    public void testSplitArray() {
+    public void testcase1() {
         int[] nums = {7, 2, 5, 10, 8};
         int m = 2;
-        int res = 18;
-        assertEquals(res, splitArray(nums, m));
+        test(nums, m, 18);
     }
 
-    public static void main(String[] args) {
-        int[] nums = {7, 2, 5, 10, 8};
+    @Test
+    public void testcase2() {
+        int[] nums = {1, 2147483647};
         int m = 2;
+        test(nums, m, 2147483647);
+    }
+
+    @Test
+    public void testcase3() {
+        int[] nums = {1, 4, 4};
+        int m = 3;
+        test(nums, m, 4);
+    }
+
+    private void test(int[] nums, int m, int expected) {
         Method method = getMethod();
-        System.out.println(method.splitArray(nums, m));
+        int actual = method.splitArray(nums, m);
+        assertThat(actual, is(expected));
     }
 }
--- src/subjects/BinarySearch.md
+++ src/subjects/BinarySearch.md
@@ -1,5 +1,5 @@
 # Binary Search (Binary Trial):
-##### 1. [LeetCode 410. Split Array Largest Sum](/src/leetcode/p401to450/LeetCode410SplitArrayLargestSum.java) - Same with: LintCode437CopyBook
+##### 1. [LeetCode 410. Split Array Largest Sum](/src/leetcode/p401to450/LeetCode410SplitArrayLargestSum.java)
 ##### 2. [LeetCode 875. Koko Eating Bananas](/src/leetcode/p851to900/LeetCode875KokoEatingBananas.java)
 ##### 3. [LeetCode 774. Minimize Max Distance To Gas Station](/src/leetcode/p751to800/LeetCode774MinimizeMaxDistanceToGasStation.java)
 ##### 4. [LeetCode 644. Maximum Average Subarray II](/src/leetcode/p601to650/LeetCode644MaximumAverageSubarrayII.java)
