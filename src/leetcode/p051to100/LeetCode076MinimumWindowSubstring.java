265c1fd26cd552d683ccfc90d7225dbf3e8a2d76
Ray Huang <huang.jingze@columbia.edu>
2019-09-05 22:05:54 -0700

    [Mod] LeetCode076MinimumWindowSubstring

--- src/leetcode/p051to100/LeetCode076MinimumWindowSubstring.java
+++ src/leetcode/p051to100/LeetCode076MinimumWindowSubstring.java
@@ -2,32 +2,40 @@ package leetcode.p051to100;
 
 public class LeetCode076MinimumWindowSubstring {
 
-    public String minWindow(String s, String t) {
-        if (s == null || s.isEmpty() || t == null || t.isEmpty()) {
-            return "";
-        }
-        int start = 0;
-        int minStart = 0;
-        int minLen = Integer.MAX_VALUE;
-        int deficiency = t.length();
-        int[] map = new int[128];
-        for (int i = 0, tLen = t.length(); i < tLen; i++) {
-            map[t.charAt(i)]++;
-        }
-        for (int end = 0, sLen = s.length(); end < sLen; end++) {
-            if (map[s.charAt(end)]-- > 0) {
-                deficiency--;
+    private interface Method {
+        String minWindow(String s, String t);
+    }
+
+    private static class Greedy implements  Method {
+
+        @Override
+        public String minWindow(String s, String t) {
+            if (s == null || s.isEmpty() || t == null || t.isEmpty()) {
+                return "";
             }
-            while (deficiency == 0) {
-                if ((end - start + 1) < minLen) {
-                    minLen = end - start + 1;
-                    minStart = start;
+            int start = 0;
+            int minStart = 0;
+            int minLen = Integer.MAX_VALUE;
+            int deficiency = t.length();
+            int[] map = new int[128];
+            for (char c : t.toCharArray()) {
+                map[c]++;
+            }
+            for (int cursor = 0; cursor < s.length(); cursor++) {
+                if (map[s.charAt(cursor)]-- > 0) {
+                    deficiency--;
                 }
-                if (map[s.charAt(start++)]++ == 0) {
-                    deficiency++;
+                while (deficiency == 0) {
+                    if ((cursor - start + 1) < minLen) {
+                        minLen = cursor - start + 1;
+                        minStart = start;
+                    }
+                    if (map[s.charAt(start++)]++ >= 0) {
+                        deficiency++;
+                    }
                 }
             }
+            return minLen <= s.length() ? s.substring(minStart, minStart + minLen) : "";
         }
-        return minLen <= s.length() ? s.substring(minStart, minStart + minLen) : "";
     }
 }
