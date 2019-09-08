d6a607b6cb3832d93f0ac39792a2b46544b14648
Ray Huang <huang.jingze@columbia.edu>
2019-09-08 00:01:34 -0700

    [Mod] LeetCode139WordBreak

--- src/leetcode/p101to150/LeetCode139WordBreak.java
+++ src/leetcode/p101to150/LeetCode139WordBreak.java
@@ -1,5 +1,7 @@
 package leetcode.p101to150;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -7,50 +9,79 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class LeetCode139WordBreak {
-  
-  private List<Integer> getLen(List<String> dict) {
-    Set<Integer> set = new HashSet<>();
-    for (int i = 0, len = dict.size(); i < len; i++) {
-      set.add(dict.get(i).length());
-    }
-    List<Integer> list = new ArrayList<>(set);
-    Collections.sort(list);
-    return list;
-  }
-  
-  public boolean wordBreak(String s, List<String> wordDict) {
-    if (s == null || s.length() == 0) {
-      return false;
-    }
-    if (wordDict == null || wordDict.size() == 0) {
-      return false;
-    }
-    int len = s.length();
-    boolean[] dp = new boolean[len + 1];
-    dp[0] = true;
-    List<Integer> lenList = getLen(wordDict);
-    for (int i = 1; i <= len; i++) {
-      for (Integer wLen : lenList) {
-        if (wLen > i) { break; }
-        if (!dp[i - wLen]) { continue; }
-        String word = s.substring(i - wLen, i);
-        dp[i] |= wordDict.contains(word);
-      }
-    }
-    return dp[len];
-  }
 
-  public static void main(String[] args) {
-    //Expected Result: true.
-    String s = "abcd";
-    List<String> wordDict = Arrays.asList("a","abc","b","cd");
+    private interface Method {
+        boolean wordBreak(String s, List<String> wordDict);
+    }
 
-////    Expected Result: true.
-//    String s = "applepenapple";
-//    List<String> wordDict = Arrays.asList("apple","pen");
-        LeetCode139WordBreak one = new LeetCode139WordBreak();
-   System.out.println(one.wordBreak(s, wordDict));
-  }
+    private static final class DP implements Method {
 
+        @Override
+        public boolean wordBreak(String s, List<String> wordDict) {
+            if (s == null || s.length() == 0 || wordDict == null || wordDict.size() == 0) {
+                return false;
+            }
+            boolean[] dp = new boolean[s.length() + 1];
+            dp[0] = true;
+            List<Integer> lenList = getLengthList(wordDict);
+            for (int i = 1; i <= s.length(); i++) {
+                for (Integer wLen : lenList) {
+                    if (wLen > i) {
+                        break;
+                    }
+                    if (!dp[i - wLen]) {
+                        continue;
+                    }
+                    String word = s.substring(i - wLen, i);
+                    dp[i] |= wordDict.contains(word);
+                }
+            }
+            return dp[dp.length - 1];
+        }
+
+        private List<Integer> getLengthList(List<String> dict) {
+            Set<Integer> set = new HashSet<>();
+            for (String s : dict) {
+                set.add(s.length());
+            }
+            List<Integer> list = new ArrayList<>(set);
+            Collections.sort(list);
+            return list;
+        }
+    }
+
+    private Method getMethod() {
+        return new DP();
+    }
+
+    @Test
+    public void testcase1() {
+        String s = "abcd";
+        List<String> wordDict = Arrays.asList("a", "abc", "b", "cd");
+        test(s, wordDict, true);
+    }
+
+    @Test
+    public void testcase2() {
+        String s = "applepenapple";
+        List<String> wordDict = Arrays.asList("apple", "pen");
+        test(s, wordDict, true);
+    }
+
+    @Test
+    public void testcase3() {
+        String s = "applepenappla";
+        List<String> wordDict = Arrays.asList("apple", "pen");
+        test(s, wordDict, false);
+    }
+
+    private void test(String s, List<String> wordDict, boolean expected) {
+        Method method = getMethod();
+        boolean actual = method.wordBreak(s, wordDict);
+        assertThat(actual, is(expected));
+    }
 }
