74358ed175b3567d74cead4000ee3cb98e9ad9ca
Ray Huang <huang.jingze@columbia.edu>
2019-09-08 19:47:56 -0700

    [Mod] LeetCode921MinimumAddToMakeParenthesesValid

--- src/leetcode/p901to950/LeetCode921MinimumAddToMakeParenthesesValid.java
+++ src/leetcode/p901to950/LeetCode921MinimumAddToMakeParenthesesValid.java
@@ -1,22 +1,30 @@
 package leetcode.p901to950;
 
+@SuppressWarnings("unused")
 public class LeetCode921MinimumAddToMakeParenthesesValid {
 
-  public int minAddToMakeValid(String S) {
-    int left = 0;
-    int right = 0;
-    for (char c : S.toCharArray()) {
-      if (c == '(') {
-        left++;
-      } else {
-        if (left > 0) {
-          left--;
-        } else {
-          right++;
-        }
-      }
+    private interface Method {
+        int minAddToMakeValid(String S);
     }
-    return left + right;
-  }
 
+    private static final class TwoPointers implements Method {
+
+        @Override
+        public int minAddToMakeValid(String S) {
+            int left = 0;
+            int right = 0;
+            for (char c : S.toCharArray()) {
+                if (c == '(') {
+                    left++;
+                } else {
+                    if (left > 0) {
+                        left--;
+                    } else {
+                        right++;
+                    }
+                }
+            }
+            return left + right;
+        }
+    }
 }
