07a65e61d2dcd635a5ba970211e3a24a8331e15e
Ray Huang <huang.jingze@columbia.edu>
2019-09-09 17:30:58 -0700

    [Mod] LeetCode042TrappingRainWater

--- src/leetcode/p001to050/LeetCode042TrappingRainWater.java
+++ src/leetcode/p001to050/LeetCode042TrappingRainWater.java
@@ -1,61 +1,91 @@
 package leetcode.p001to050;
 
-public class LeetCode042TrappingRainWater {
-	
-  public int trap(int[] height) {
-    if (height == null || height.length == 0) {
-      return 0;
-    }
-    int left = 0;
-    int right = height.length - 1;
-    int lHeight = height[0];
-    int rHeight = height[right];
-    int totalCapacity = 0;
-    
-    while (left < right) {
-      lHeight = Math.max(lHeight, height[left]);
-      rHeight = Math.max(rHeight, height[right]);
-      
-      if (height[left] <= height[right]) {
-        while (left < right && height[left] <= lHeight) {
-          totalCapacity += lHeight - height[left++];
-        }
-      } else {
-        while (left < right && height[right] <= rHeight) {
-          totalCapacity += rHeight - height[right--];
-        }
-      }
-    }
-    return totalCapacity;
-  }
-  
-  //This method is similar to the LeetCode135 Candy 
-  public int trap2(int[] height) {
-    if (height == null || height.length == 0) {
-      return 0;
-    }
-    int total = 0;
-    int[] leftMaxHeight = new int[height.length];
-    int[] rightMaxHeight = new int[height.length];
-    leftMaxHeight[0] = height[0];
-    rightMaxHeight[height.length - 1] = height[height.length - 1];
-    for (int i = 1; i < height.length; i++) {
-      leftMaxHeight[i] = Math.max(leftMaxHeight[i - 1], height[i]);
-    }
-    for (int i = height.length - 2; i >= 0; i--) {
-      rightMaxHeight[i] = Math.max(rightMaxHeight[i + 1], height[i]);
-    }
-    for (int i = 0; i < height.length; i++) {
-      total += Math.min(rightMaxHeight[i], leftMaxHeight[i]) - height[i];
-    }
-    return total;
-  }
-  
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    LeetCode042TrappingRainWater one = new LeetCode042TrappingRainWater();
-    int[] heights = {0,1,0,2,1,0,1,3,2,1,2,1};
-    System.out.println(one.trap2(heights));
-  }
+import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SuppressWarnings("unused")
+public class LeetCode042TrappingRainWater {
+
+    private interface Method {
+        int trap(int[] heights);
+    }
+
+    private static final class TwoPointers implements Method {
+
+        @Override
+        public int trap(int[] heights) {
+            if (heights == null || heights.length == 0) {
+                return 0;
+            }
+            int left = 0;
+            int right = heights.length - 1;
+            int lHeight = heights[0];
+            int rHeight = heights[right];
+            int res = 0;
+
+            while (left < right) {
+                lHeight = Math.max(lHeight, heights[left]);
+                rHeight = Math.max(rHeight, heights[right]);
+
+                if (heights[left] < heights[right]) {
+                    res += lHeight - heights[left++];
+                } else {
+                    res += rHeight - heights[right--];
+                }
+            }
+            return res;
+        }
+    }
+
+    /**
+     * This method is similar to the {@link leetcode.p101to150.LeetCode135Candy}
+     */
+    private static final class Candy implements Method {
+
+        @Override
+        public int trap(int[] heights) {
+            if (heights == null || heights.length == 0) {
+                return 0;
+            }
+            int total = 0;
+            int[] leftMaxHeight = new int[heights.length];
+            int[] rightMaxHeight = new int[heights.length];
+            leftMaxHeight[0] = heights[0];
+            rightMaxHeight[heights.length - 1] = heights[heights.length - 1];
+            for (int i = 1; i < heights.length; i++) {
+                leftMaxHeight[i] = Math.max(leftMaxHeight[i - 1], heights[i]);
+            }
+            for (int i = heights.length - 2; i >= 0; i--) {
+                rightMaxHeight[i] = Math.max(rightMaxHeight[i + 1], heights[i]);
+            }
+            for (int i = 0; i < heights.length; i++) {
+                total += Math.min(rightMaxHeight[i], leftMaxHeight[i]) - heights[i];
+            }
+            return total;
+        }
+    }
+
+    private static Method getMethod() {
+        return new Candy();
+    }
+
+    private void test(int[] heights, int expected) {
+        Method method = getMethod();
+        int actual = method.trap(heights);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void testcase1() {
+        int[] heights = {1, 0, 1};
+        test(heights, 1);
+    }
+
+    @Test
+    public void testcase2() {
+        int[] heights = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        test(heights, 6);
+    }
 }
