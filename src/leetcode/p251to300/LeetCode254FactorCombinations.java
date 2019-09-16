6df29b8c514375fcd86ad4dd234a5d948c1cec6d
Ray Huang <huang.jingze@columbia.edu>
2019-09-15 22:32:58 -0700

    [New] LeetCode254FactorCombinations

--- src/leetcode/p251to300/LeetCode254FactorCombinations.java
+++ src/leetcode/p251to300/LeetCode254FactorCombinations.java
@@ -5,45 +5,85 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import utils.Print;
 
+/**
+ * 254. Factor Combinations
+ * Medium
+ * <p>
+ * Numbers can be regarded as product of its factors. For example,
+ * <p>
+ * 8 = 2 x 2 x 2;
+ * = 2 x 4.
+ * Write a function that takes an integer n and return all possible combinations of its factors.
+ * <p>
+ * Note:
+ * You may assume that n is always positive.
+ * Factors should be greater than 1 and less than n.
+ * <p>
+ * Example 1:
+ * Input: 1
+ * Output: []
+ * <p>
+ * Example 2:
+ * Input: 37
+ * Output:[]
+ * <p>
+ * Example 3:
+ * Input: 12
+ * Output:
+ * [
+ * [2, 6],
+ * [2, 2, 3],
+ * [3, 4]
+ * ]
+ * <p>
+ * Example 4:
+ * Input: 32
+ * Output:
+ * [
+ * [2, 16],
+ * [2, 2, 8],
+ * [2, 2, 2, 4],
+ * [2, 2, 2, 2, 2],
+ * [2, 4, 4],
+ * [4, 8]
+ * ]
+ */
+@SuppressWarnings("unused")
 public class LeetCode254FactorCombinations {
-  
-  private Map<Integer, List<List<Integer>>> cache;
-  
-  public List<List<Integer>> getFactors(int n) {
-    cache = new HashMap<>();
-    return helper(n);
-  }
-  
-  private List<List<Integer>> helper(int n) {
-    if (cache.containsKey(n)) {
-      return cache.get(n);
+
+    private interface Method {
+        List<List<Integer>> getFactors(int n);
     }
-    List<List<Integer>> res = new ArrayList<>();    
-    for (int i = 2; i * i <= n; i++) {
-      if (n % i == 0) {
-        List<List<Integer>> sub = getFactors(n / i);
-        res.add(Arrays.asList(i, n / i));
-        for (List<Integer> list : sub) {
-          if (list.get(0) >= i) {
-            List<Integer> neo = new ArrayList<>();
-            neo.add(i);
-            neo.addAll(list);
-            res.add(neo);
-          }
+
+    private static final class Memorization implements Method {
+
+        @Override
+        public List<List<Integer>> getFactors(int n) {
+            return getFactors(n, new HashMap<>());
         }
-      }
+
+        private List<List<Integer>> getFactors(int n, Map<Integer, List<List<Integer>>> cache) {
+            if (cache.containsKey(n)) {
+                return cache.get(n);
+            }
+            List<List<Integer>> res = new ArrayList<>();
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    List<List<Integer>> sub = getFactors(n / i, cache);
+                    res.add(Arrays.asList(i, n / i));
+                    for (List<Integer> list : sub) {
+                        if (list.get(0) >= i) {
+                            List<Integer> neo = new ArrayList<>();
+                            neo.add(i);
+                            neo.addAll(list);
+                            res.add(neo);
+                        }
+                    }
+                }
+            }
+            cache.put(n, res);
+            return res;
+        }
     }
-    cache.put(n, res);
-    return res;
-  }
-
-  public static void main(String[] args) {
-    int n = 32;
-    LeetCode254FactorCombinations one = 
-        new LeetCode254FactorCombinations();
-    Print.printListList(one.getFactors(n));
-  }
-
 }
---
+++ src/subjects/DFS.md
@@ -0,0 +1,4 @@
+# DFS:
+
+### 1. Memorization
+##### 1.1. [*LeetCode 254. Factor Combinations](/src/leetcode/p251to300/LeetCode254FactorCombinations.java)
