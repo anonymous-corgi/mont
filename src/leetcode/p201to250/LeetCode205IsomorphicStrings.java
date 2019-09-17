55b1e8699b7a7beebd49d3297e95279ae5781c5f
Ray Huang <huang.jingze@columbia.edu>
2019-09-16 19:54:25 -0700

    [Mod] LeetCode205IsomorphicStrings

--- src/leetcode/p201to250/LeetCode205IsomorphicStrings.java
+++ src/leetcode/p201to250/LeetCode205IsomorphicStrings.java
@@ -1,10 +1,34 @@
 package leetcode.p201to250;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+/**
+ * 205. Isomorphic Strings
+ * Easy
+ * <p>
+ * Given two strings s and t, determine if they are isomorphic.
+ * <p>
+ * Two strings are isomorphic if the characters in s can be replaced to get t.
+ * <p>
+ * All occurrences of a character must be replaced with another character
+ * while preserving the order of characters. No two characters may map to the same character
+ * but a character may map to itself.
+ * <p>
+ * Example 1:
+ * Input: s = "egg", t = "add"
+ * Output: true
+ * <p>
+ * Example 2:
+ * Input: s = "foo", t = "bar"
+ * Output: false
+ * <p>
+ * Example 3:
+ * Input: s = "paper", t = "title"
+ * Output: true
+ */
 public class LeetCode205IsomorphicStrings {
 
     private interface Method {
@@ -17,10 +41,12 @@ public class LeetCode205IsomorphicStrings {
         public boolean isIsomorphic(String s, String t) {
             int[] m = new int[256];
             for (int i = 0; i < s.length(); i++) {
-                if (m[s.charAt(i)] != m[t.charAt(i) + 128]) {
+                int sp = s.charAt(i);
+                int tp = t.charAt(i) + 128;
+                if (m[sp] != m[tp]) {
                     return false;
                 }
-                m[s.charAt(i)] = m[t.charAt(i) + 128] = i + 1;
+                m[sp] = m[tp] = i + 1;
             }
             return true;
         }
@@ -50,6 +76,11 @@ public class LeetCode205IsomorphicStrings {
         return new PositionMap();
     }
 
+    private void test(String s, String t, boolean expect) {
+        Method method = getMethod();
+        assertThat(method.isIsomorphic(s, t), is(expect));
+    }
+
     @Test
     public void testcase1() {
         test("egg", "add", true);
@@ -60,8 +91,8 @@ public class LeetCode205IsomorphicStrings {
         test("ab", "aa", false);
     }
 
-    private void test(String s, String t, boolean expect) {
-        Method method = getMethod();
-        Assert.assertThat(method.isIsomorphic(s, t), is(expect));
+    @Test
+    public void testcase3() {
+        test("aa", "ab", false);
     }
 }
