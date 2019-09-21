19205b77f1e2a5f17f4833da61d095086cde1352
Ray Huang <huang.jingze@columbia.edu>
2019-09-21 01:32:52 -0700

    [Mod] LeetCode307RangeSumQueryMutable

--- src/leetcode/p301to350/LeetCode307RangeSumQueryMutable.java
+++ src/leetcode/p301to350/LeetCode307RangeSumQueryMutable.java
@@ -1,45 +1,47 @@
 package leetcode.p301to350;
 
 public class LeetCode307RangeSumQueryMutable {
-  
-  public static class BinaryIndexedTree_Method {
-    
-    private final int[] bit;
-    private final int[] nums;
-    public BinaryIndexedTree_Method(int[] nums) {
-//    public NumArray(int[] nums) {
-      this.bit = new int[nums.length + 1];
-      this.nums = new int[nums.length + 1];
-      for (int i = 0; i < nums.length; i++) {
-        update(i, nums[i]);
-      }
-    }
-    
-    public void update(int i, int val) {
-      int diff = val - nums[++i];
-      nums[i] = val;
-      for (; i < bit.length; i += (i & -i)) {
-        bit[i] += diff;
-      }
-    }
-    
-    public int sumRange(int i, int j) {
-      return getSum(j + 1) - getSum(i);
-    }
-    
-    private int getSum(int i) {
-      int res = 0;
-      for (; i > 0; i -= (i & -i)) {
-        res += bit[i];
-      }
-      return res;
-    }
-    
-  }
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    private static abstract class NumArray {
 
-  }
+        abstract public void update(int i, int val);
 
+        abstract public int sumRange(int i, int j);
+    }
+
+    private static final class BinaryIndexedTree extends NumArray {
+
+        private final int[] bits;
+        private final int[] stub;
+
+        public BinaryIndexedTree(int[] nums) {
+            this.bits = new int[nums.length + 1];
+            this.stub = new int[nums.length + 1];
+            for (int i = 0; i < nums.length; i++) {
+                update(i, nums[i]);
+            }
+        }
+
+        public void update(int i, int val) {
+            int index = i + 1;
+            int diff = val - stub[index];
+            stub[index] = val;
+            for (; index < bits.length; index += (index & -index)) {
+                bits[index] += diff;
+            }
+        }
+
+        public int sumRange(int i, int j) {
+            return getSum(j) - getSum(i - 1);
+        }
+
+        private int getSum(int i) {
+            int index = i + 1;
+            int sum = 0;
+            for (; index > 0; index -= (index & -index)) {
+                sum += bits[index];
+            }
+            return sum;
+        }
+    }
 }
