7521ef0bf101de1b05a0606218fba52262029312
Ray Huang <huang.jingze@columbia.edu>
2019-09-21 10:28:08 -0700

    [Mod] LeetCode308RangeSumQuery2DMutable

--- src/leetcode/p301to350/LeetCode308RangeSumQuery2DMutable.java
+++ src/leetcode/p301to350/LeetCode308RangeSumQuery2DMutable.java
@@ -1,60 +1,53 @@
 package leetcode.p301to350;
 
 public class LeetCode308RangeSumQuery2DMutable {
-  
-  private int rows, cols;
-  private final int[][] stub;
-  private final int[][] bit;
-  
-  public LeetCode308RangeSumQuery2DMutable(int[][] matrix) {
-      rows = matrix.length;
-      cols = matrix[0].length;
-      stub = new int[rows][cols];
-      bit = new int[rows + 1][cols + 1];
-      
-      for (int i = 0; i < rows; i++) {
-          for (int j = 0; j < cols; j++) {
-              update(i, j, matrix[i][j]);
-          }
-      }
-  }
-  
-  public void update(int row, int col, int val) {
-      int delta = val - stub[row][col];
-      stub[row][col] = val;
-      
-      for (int i = row + 1; i <= rows; i = i + lowbit(i)) {
-          for (int j = col + 1; j <= cols; j = j + lowbit(j)) {
-              bit[i][j] += delta;
-          }
-      }
-  }
-  
-  public int sumRegion(int row1, int col1, int row2, int col2) {
-      return (
-          prefixSum(row2, col2) -
-          prefixSum(row2, col1 - 1) -
-          prefixSum(row1 - 1, col2) +
-          prefixSum(row1 - 1, col1 - 1)
-      );
-  }
-  
-  private int prefixSum(int row, int col) {
-      int sum = 0;
-      for (int i = row + 1; i > 0; i = i - lowbit(i)) {
-          for (int j = col + 1; j > 0; j = j - lowbit(j)) {
-              sum += bit[i][j];
-          }
-      }
-      return sum;
-  }
-  
-  private int lowbit(int x) {
-      return x & -x;
-  }
 
-  public static void main(String[] args) {
-    
-  }
+    private static abstract class NumMatrix {
 
+        abstract public void update(int row, int col, int val);
+
+        abstract public int sumRegion(int row1, int col1, int row2, int col2);
+    }
+
+    private static final class BinaryIndexedTree extends NumMatrix {
+        private final int[][] stub;
+        private final int[][] bit;
+
+        public BinaryIndexedTree(int[][] matrix) {
+            int rows = matrix.length + 1;
+            int cols = rows == 1 ? 1 : matrix[0].length + 1;
+            stub = new int[rows][cols];
+            bit = new int[rows][cols];
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    update(i, j, matrix[i][j]);
+                }
+            }
+        }
+
+        public void update(int row, int col, int val) {
+            int diff = val - stub[row + 1][col + 1];
+            stub[row + 1][col + 1] = val;
+            for (int i = row + 1; i < bit.length; i += (i & -i)) {
+                for (int j = col + 1; j < bit[i].length; j += (j & -j)) {
+                    bit[i][j] += diff;
+                }
+            }
+        }
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            return getSum(row2, col2) - getSum(row2, col1 - 1) -
+                    getSum(row1 - 1, col2) + getSum(row1 - 1, col1 - 1);
+        }
+
+        private int getSum(int row, int col) {
+            int sum = 0;
+            for (int i = row + 1; i > 0; i -= (i & -i)) {
+                for (int j = col + 1; j > 0; j -= (j & -j)) {
+                    sum += bit[i][j];
+                }
+            }
+            return sum;
+        }
+    }
 }
