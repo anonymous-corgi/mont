9b7cad02c79808555b956966fb303828daea2ce3
Ray Huang <huang.jingze@columbia.edu>
2019-09-22 14:08:26 -0700

    [Mod] LeetCode140WordBreakII

--- src/leetcode/p101to150/LeetCode140WordBreakII.java
+++ src/leetcode/p101to150/LeetCode140WordBreakII.java
@@ -1,58 +1,55 @@
 package leetcode.p101to150;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class LeetCode140WordBreakII {
-  
-  private HashMap<String, List<String>> cache;
-  
-  public List<String> wordBreak(String s, List<String> wordDict) {
-    cache = new HashMap<String, List<String>>();
-    cache.put("", Arrays.asList(""));
-    List<Integer> lenList = getLenList(wordDict);
-    return dfs(s, lenList, new HashSet<String>(wordDict));
-  }       
-  
-  private List<String> dfs(String s, List<Integer> lenList, Set<String> wordDict) {
-    if (cache.containsKey(s)) {
-      return cache.get(s);
+
+    private interface Method {
+        List<String> wordBreak(String s, List<String> wordDict);
     }
-    
-    List<String> res = new ArrayList<String>();
-    for (Integer wLen : lenList) {
-      if (wLen > s.length()) {
-        break;
-      }
-      String head = s.substring(0, wLen);
-      if (wordDict.contains(head)) {
-        List<String> sublist = dfs(s.substring(wLen), lenList, wordDict);
-        for (String sub : sublist) {
-          res.add(head + (sub.length() == 0? "" : " " + sub));
+
+    private static final class Memorization implements Method {
+
+        public List<String> wordBreak(String s, List<String> wordDict) {
+            List<Integer> lenList = getLenList(wordDict);
+            return dfs(s, lenList, new HashSet<>(wordDict), new HashMap<>());
         }
-      }
+
+        private List<String> dfs(String s, List<Integer> lenList, Set<String> wordDict,
+                                 Map<String, List<String>> cache) {
+            if (cache.containsKey(s)) {
+                return cache.get(s);
+            }
+            List<String> result = new ArrayList<>();
+            for (int len : lenList) {
+                if (len > s.length()) {
+                    break;
+                }
+                String subStr = s.substring(0, len);
+                if (wordDict.contains(subStr)) {
+                    if (len < s.length()) {
+                        List<String> rest = dfs(s.substring(len), lenList, wordDict, cache);
+                        for (String restStr : rest) {
+                            result.add(subStr + " " + restStr);
+                        }
+                    } else {
+                        result.add(subStr);
+                        break;
+                    }
+                }
+            }
+            cache.put(s, result);
+            return result;
+        }
+
+        private List<Integer> getLenList(List<String> wordDict) {
+            Set<Integer> lenSet = new HashSet<>();
+            for (String word : wordDict) {
+                lenSet.add(word.length());
+            }
+            List<Integer> lenList = new ArrayList<>(lenSet);
+            Collections.sort(lenList);
+            return lenList;
+        }
     }
-    cache.put(s, res);
-    return res;
-  }
-  
-  private List<Integer> getLenList(List<String> wordDict) {
-    Set<Integer> set = new HashSet<>();
-    for (String str : wordDict) {
-      set.add(str.length());
-    }
-    List<Integer> list = new ArrayList<>(set);
-    Collections.sort(list);
-    return list;
-  }
-
-  public static void main(String[] args) {
-
-  }
-
 }
--- src/subjects/DFS.md
+++ src/subjects/DFS.md
@@ -1,7 +1,9 @@
 # DFS:
 
 ### 1. Memorization
-##### 1.1. [*LeetCode 254. Factor Combinations](/src/leetcode/p251to300/LeetCode254FactorCombinations.java)
+##### 1.1. [LeetCode 139. Word Break](/src/leetcode/p101to150/LeetCode139WordBreak.java)
+##### 1.2. [LeetCode 140. Word Break II](/src/leetcode/p101to150/LeetCode140WordBreakII.java)
+##### 1.3. [*LeetCode 254. Factor Combinations](/src/leetcode/p251to300/LeetCode254FactorCombinations.java)
 
 ### 2. Partition
 ##### 2.1. [*LeetCode 698. Partition to K Equal Sum Subsets](/src/leetcode/p651to700/LeetCode698PartitionToKEqualSumSubsets.java)
