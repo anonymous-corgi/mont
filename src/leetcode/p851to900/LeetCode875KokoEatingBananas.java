098472ed4a94d7162053b349feb6969bf94b00f9
Ray Huang <huang.jingze@columbia.edu>
2019-09-23 21:03:54 -0700

    [Mod] LeetCode875KokoEatingBananas

--- src/leetcode/p851to900/LeetCode875KokoEatingBananas.java
+++ src/leetcode/p851to900/LeetCode875KokoEatingBananas.java
@@ -1,43 +1,37 @@
 package leetcode.p851to900;
 
 public class LeetCode875KokoEatingBananas {
-  
-  public int minEatingSpeed(int[] piles, int H) {
-    int start = 1;
-    int end = piles[0];
-    for (int i = 1, len = piles.length; i < len; i++) {
-      if (piles[i] > end) {
-        end = piles[i];
-      }
-    }
-        
-    while (start + 1 < end) {
-      int mid = (start + end) / 2;
-      if (canFinish(piles, mid, H)) {
-        end = mid;
-      } else {
-        start = mid;
-      }            
-    }
-    
-    if (canFinish(piles, start, H)) {
-      return start;
-    } else {
-      return end;
-    }
-  }
-  
-  private boolean canFinish(int[] piles, int k, int h) {
-    int t = 0;
-    for (int i = 0, len = piles.length; i < len; i++) {
-      t += (piles[i] % k == 0 ? piles[i] / k : piles[i] / k + 1);
-    }
-    return t <= h;
-  }
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    private interface Method {
+        int minEatingSpeed(int[] piles, int H);
+    }
 
-  }
+    private static final class BinaryTrial implements Method {
 
+        public int minEatingSpeed(int[] piles, int H) {
+            int begin = 1;
+            int end = 0;
+            for (int pile : piles) {
+                end = Math.max(end, pile);
+            }
+
+            while (begin < end) {
+                int mid = begin + (end - begin) / 2;
+                if (canFinish(piles, mid, H)) {
+                    end = mid;
+                } else {
+                    begin = mid + 1;
+                }
+            }
+            return end;
+        }
+
+        private boolean canFinish(int[] piles, int k, int h) {
+            int t = 0;
+            for (int pile : piles) {
+                t += (pile % k == 0 ? pile / k : pile / k + 1);
+            }
+            return t <= h;
+        }
+    }
 }
