b15d10f330b663fa3a71f5ed6320fba7135e9209
Ray Huang <huang.jingze@columbia.edu>
2019-09-26 00:22:32 -0700

    [Mod] LeetCode218TheSkylineProblem

--- src/leetcode/p201to250/LeetCode218TheSkylineProblem.java
+++ src/leetcode/p201to250/LeetCode218TheSkylineProblem.java
@@ -1,58 +1,61 @@
 package leetcode.p201to250;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.TreeMap;
 
 public class LeetCode218TheSkylineProblem {
 
     private interface Method {
-        List<int[]> getSkyline(int[][] buildings);
+        List<List<Integer>> getSkyline(int[][] buildings);
     }
 
-    private static class Point_TreeMap_Method implements Method{
+    private static final class Point_TreeMap implements Method {
 
-        @Override
-        public List<int[]> getSkyline(int[][] buildings) {
-            List<int[]> res = new ArrayList<>();
-            List<int[]> height = new ArrayList<>();
-            for (int[] each : buildings) {
-                height.add(new int[]{each[0], each[2]});
-                height.add(new int[]{each[1], -each[2]});
+        private static final class Point {
+            int pos;
+            int height;
+
+            private Point(int pos, int height) {
+                this.pos = pos;
+                this.height = height;
             }
-            height.sort((a, b) -> a[0] != b[0] ? a[0] - b[0] : b[1] - a[1]);
+        }
+
+        public List<List<Integer>> getSkyline(int[][] buildings) {
+            List<List<Integer>> result = new ArrayList<>();
+            List<Point> points = new ArrayList<>();
+            for (int[] building : buildings) {
+                points.add(new Point(building[0], building[2]));
+                points.add(new Point(building[1], -building[2]));
+            }
+            points.sort((a, b) -> a.pos != b.pos ? a.pos - b.pos : b.height - a.height);
             TreeMap<Integer, Integer> highest = new TreeMap<>();
             highest.put(0, 1);
-            int prev = 0;
-            for (int[] h : height) {
-                if (h[1] > 0) {
-                    highest.put(h[1], highest.getOrDefault(h[1], 0) + 1);
+            int lastHighest = 0;
+            for (Point point : points) {
+                if (point.height > 0) {
+                    highest.compute(point.height, this::increase);
                 } else {
-                    int time = highest.get(-h[1]) - 1;
-                    if (time == 0) {
-                        highest.remove(-h[1]);
-                    } else {
-                        highest.put(-h[1], time);
-                    }
+                    highest.compute(-point.height, this::decrease);
                 }
-                int cur = highest.lastKey();
-                if (cur != prev) {
-                    res.add(new int[]{h[0], cur});
-                    prev = cur;
+                int currentHighest = highest.lastKey();
+                if (currentHighest != lastHighest) {
+                    result.add(Arrays.asList(point.pos, currentHighest));
+                    lastHighest = currentHighest;
                 }
             }
-            return res;
+            return result;
         }
-    }
 
-    public static void main(String[] args) {
-        int[][] buildings = {{0, 2, 3}, {2, 5, 3}};
-//    int[][] buildings = {{2,9,10},{3,7,15},{5,12,12},{15,20,10},{19,24,8}};
-        Method one = new Point_TreeMap_Method();
-        List<int[]> res = one.getSkyline(buildings);
-        for (int i = 0, iLen = res.size(); i < iLen; i++) {
-            int[] point = res.get(i);
-            System.out.print("[" + point[0] + ", " + point[1] + "]\t");
+        private Integer increase(Integer k, Integer v) {
+            return v == null ? 1 : v + 1;
+        }
+
+        // Return null cause compute removes the Map.Entry.
+        private Integer decrease(Integer k, Integer v) {
+            return v - 1 == 0 ? null : v - 1;
         }
     }
 }
