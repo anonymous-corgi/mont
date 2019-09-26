371df1e0bcdf9a98483de358e50e6ffefb66f102
Ray Huang <huang.jingze@columbia.edu>
2019-09-25 23:41:50 -0700

    [Mod] LeetCode732MyCalendarIII

--- src/leetcode/p701to750/LeetCode732MyCalendarIII.java
+++ src/leetcode/p701to750/LeetCode732MyCalendarIII.java
@@ -4,33 +4,28 @@ import java.util.TreeMap;
 
 public class LeetCode732MyCalendarIII {
 
-    private interface Method {
-        int book(int start, int end);
+    private static abstract class MyCalendarThree {
+
+        public MyCalendarThree() {
+        }
+
+        abstract public int book(int start, int end);
     }
 
-    private static class TreeMap_Method implements Method {
+    private static final class TreeMapImpl extends MyCalendarThree {
 
-        private TreeMap<Integer, Integer> timeline = new TreeMap<>();
+        private final TreeMap<Integer, Integer> events = new TreeMap<>();
 
-        @Override
-        public int book(int s, int e) {
-            timeline.put(s, timeline.getOrDefault(s, 0) + 1);
-            timeline.put(e, timeline.getOrDefault(e, 0) - 1);
+        public int book(int start, int end) {
+            events.compute(start, (k, v) -> v == null ? 1 : v + 1);
+            events.compute(end, (k, v) -> v == null ? -1 : v - 1);
             int max = 0;
             int count = 0;
-            for (int v : timeline.values()) {
+            for (int v : events.values()) {
                 count += v;
                 max = Math.max(max, count);
             }
             return max;
-        }
-    }
-
-    public static void main(String[] args) {
-        Method one = new TreeMap_Method();
-        int[][] times = {{10, 11}, {10, 13}, {5, 6}, {3, 6}, {4, 13}, {1, 16}};
-        for (int[] time : times) {
-            System.out.println(one.book(time[0], time[1]));
         }
     }
 }
