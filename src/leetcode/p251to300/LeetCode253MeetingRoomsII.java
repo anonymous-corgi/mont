23df85efde72b42bba6fb6061793ed4f6a6df215
Ray Huang <huang.jingze@columbia.edu>
2019-09-25 19:49:35 -0700

    [Mod] LeetCode253MeetingRoomsII

--- src/leetcode/p251to300/LeetCode253MeetingRoomsII.java
+++ src/leetcode/p251to300/LeetCode253MeetingRoomsII.java
@@ -1,25 +1,28 @@
 package leetcode.p251to300;
 
-import basicclass.Interval;
-
 import java.util.Arrays;
 
 /**
- * LeetCode 253. Meeting Rooms II
+ * 253. Meeting Rooms II
  * Medium
- * <p>
- * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+ *
+ * Given an array of meeting time intervals consisting of
+ * start and end times [[s1,e1],[s2,e2],...] (si < ei),
  * find the minimum number of conference rooms required.
- * <p>
- * For example,
- * Given [[0, 30],[5, 10],[15, 20]],
- * return 2.
+ *
+ * Example 1:
+ * Input: [[0, 30],[5, 10],[15, 20]]
+ * Output: 2
+ *
+ * Example 2:
+ * Input: [[7,10],[2,4]]
+ * Output: 1
  */
 @SuppressWarnings("unused")
-public class LeetCode253MeetingRoomII {
+public class LeetCode253MeetingRoomsII {
 
     private interface Method {
-        int minMeetingRooms(Interval[] intervals);
+        int minMeetingRooms(int[][] intervals);
     }
 
     private static class SpiteAndSort implements Method {
@@ -33,14 +36,12 @@ public class LeetCode253MeetingRoomII {
                 this.flag = flag;
             }
 
-            @Override
             public int compareTo(Point o) {
                 return time != o.time ? time - o.time : flag - o.flag;
             }
         }
 
-        @Override
-        public int minMeetingRooms(Interval[] intervals) {
+        public int minMeetingRooms(int[][] intervals) {
             if (intervals == null || intervals.length == 0) {
                 return 0;
             }
@@ -48,12 +49,11 @@ public class LeetCode253MeetingRoomII {
             int count = 0;
             Point[] points = new Point[intervals.length * 2];
             for (int i = 0; i < points.length; i += 2) {
-                Interval interval = intervals[i / 2];
-                points[i] = new Point(interval.start, 1);
-                points[i + 1] = new Point(interval.end, -1);
+                int[] interval = intervals[i / 2];
+                points[i] = new Point(interval[0], 1);
+                points[i + 1] = new Point(interval[1], -1);
             }
             Arrays.sort(points);
-
             for (Point point : points) {
                 count += point.flag;
                 res = Math.max(res, count);
