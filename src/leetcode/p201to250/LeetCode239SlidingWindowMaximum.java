d4e851c74c64ca945c6479e8a19780561b55497a
Ray Huang <huang.jingze@columbia.edu>
2019-09-28 13:40:47 -0700

    [Mod] LeetCode239SlidingWindowMaximum

--- src/leetcode/p201to250/LeetCode239SlidingWindowMaximum.java
+++ src/leetcode/p201to250/LeetCode239SlidingWindowMaximum.java
@@ -3,29 +3,36 @@ package leetcode.p201to250;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * Similar to {@link leetcode.p901to950.LeetCode918MaximumSumCircularSubarray}
+ */
 public class LeetCode239SlidingWindowMaximum {
-  
-  public int[] maxSlidingWindow(int[] nums, int k) {
-    if (nums == null || nums.length < k || nums.length == 0) {
-      return new int[0];
-    }
-    int[] res = new int[nums.length - k + 1];
-    Deque<Integer> maxQue = new ArrayDeque<>(k);
-    for (int i = 0; i < nums.length; i++) {
-      while (!maxQue.isEmpty() && maxQue.peekFirst() <= i - k) {
-        maxQue.pollFirst();
-      }
-      
-      while (!maxQue.isEmpty() && nums[maxQue.peekLast()] < nums[i]) {
-        maxQue.pollLast();
-      }
-      
-      maxQue.offer(i);
-      if (i >= k - 1) {
-        res[i - k + 1] = nums[maxQue.peekFirst()];
-      }
-    }
-    return res;
-  }
 
+    private interface Method {
+        int[] maxSlidingWindow(int[] nums, int k);
+    }
+
+    private static final class Dequeue implements Method {
+
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            if (nums == null || nums.length < k || nums.length == 0) {
+                return new int[0];
+            }
+            int[] res = new int[nums.length - k + 1];
+            Deque<Integer> maxCandidates = new ArrayDeque<>(k + 1);
+            for (int i = 0; i < nums.length; i++) {
+                while (!maxCandidates.isEmpty() && i - maxCandidates.peekFirst() >= k) {
+                    maxCandidates.pollFirst();
+                }
+                while (!maxCandidates.isEmpty() && nums[maxCandidates.peekLast()] <= nums[i]) {
+                    maxCandidates.pollLast();
+                }
+                maxCandidates.offer(i);
+                if (i >= k - 1) {
+                    res[i - k + 1] = nums[maxCandidates.peekFirst()];
+                }
+            }
+            return res;
+        }
+    }
 }
