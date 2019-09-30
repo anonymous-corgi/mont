34c12266c09f968e2f74b1871d219dd579081d56
Ray Huang <huang.jingze@columbia.edu>
2019-09-29 21:16:49 -0700

    [Mod] LeetCode496NextGreaterElementI

--- src/leetcode/p451to500/LeetCode496NextGreaterElementI.java
+++ src/leetcode/p451to500/LeetCode496NextGreaterElementI.java
@@ -1,37 +1,71 @@
 package leetcode.p451to500;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
-import utils.Print;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(Parameterized.class)
 public class LeetCode496NextGreaterElementI {
-  
-  public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-    Map<Integer, Integer> map = new HashMap<>();
-    Stack<Integer> stack = new Stack<>();
-    int[] res = new int[nums1.length];
-    for (int i = 0; i < nums2.length; i++) {
-      if (stack.isEmpty() || nums2[stack.peek()] >= nums2[i]) {
-        stack.push(i);
-      } else {
-        map.put(nums2[stack.pop()], nums2[i]);
-        i--;
-      }
+    private final int[] nums1;
+    private final int[] nums2;
+    private final int[] expected;
+
+    public LeetCode496NextGreaterElementI(int[] nums1, int[] nums2, int[] expected) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        this.expected = expected;
     }
-    for (int i = 0; i < nums1.length; i++) {
-      res[i] = map.getOrDefault(nums1[i], -1);
+
+    private interface Method {
+        int[] nextGreaterElement(int[] nums1, int[] nums2);
     }
-    return res;
-  }
 
-  public static void main(String[] args) {
-    LeetCode496NextGreaterElementI one =
-        new LeetCode496NextGreaterElementI();
-    int[] nums1 = {4,1,2};
-    int[] nums2 = {1,3,4,2};
-    Print.printArray(one.nextGreaterElement(nums1, nums2));
+    private static final class StackImpl implements Method {
 
-  }
+        public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+            Map<Integer, Integer> nextGreaterMap = new HashMap<>(nums2.length);
+            Stack<Integer> greaterStack = new Stack<>();
+            int[] res = new int[nums1.length];
+            for (int i = nums2.length - 1; i >= 0; i--) {
+                while (!greaterStack.isEmpty() && nums2[i] >= greaterStack.peek()) {
+                    greaterStack.pop();
+                }
+                int nextGreaterNum = greaterStack.isEmpty() ? -1 : greaterStack.peek();
+                nextGreaterMap.put(nums2[i], nextGreaterNum);
+                greaterStack.push(nums2[i]);
+            }
+            for (int i = 0; i < nums1.length; i++) {
+                res[i] = nextGreaterMap.get(nums1[i]);
+            }
+            return res;
+        }
+    }
 
+    private static Method getMethod() {
+        return new StackImpl();
+    }
+
+    private void test(int[] nums1, int[] nums2, int[] expected) {
+        int[] actual = getMethod().nextGreaterElement(nums1, nums2);
+        assertThat(actual, equalTo(expected));
+    }
+
+    @Test
+    public void testcase() {
+        test(nums1, nums2, expected);
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] parameters() {
+        return new Object[][]{
+                {new int[]{4, 1, 2}, new int[]{1, 3, 4, 2}, new int[]{-1, 3, -1}},
+        };
+    }
 }
