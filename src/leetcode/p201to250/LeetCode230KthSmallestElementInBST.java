1293ee944dda18c3b761c3e77e0a34ae7003c507
Ray Huang <huang.jingze@columbia.edu>
2019-09-29 20:29:18 -0700

    [Mod] LeetCode230KthSmallestElementInBST

--- src/leetcode/p201to250/LeetCode230KthSmallestElementInBST.java
+++ src/leetcode/p201to250/LeetCode230KthSmallestElementInBST.java
@@ -1,32 +1,57 @@
 package leetcode.p201to250;
 
-import java.util.Stack;
 import basicclass.TreeNode;
 
+import java.util.Stack;
+
 public class LeetCode230KthSmallestElementInBST {
-  
-  public int kthSmallest(TreeNode root, int k) {
-    Stack<TreeNode> stack = new Stack<>();
-    while (root != null) {
-      stack.push(root);
-      root = root.left;
+
+    private interface Method {
+        int kthSmallest(TreeNode root, int k);
     }
-    
-    for (int i = 0; i < k - 1; i++) {
-      TreeNode cursor = stack.pop();
-      TreeNode right = cursor.right;
-      while (right != null) {
-        stack.push(right);
-        right = right.left;
-      }
+
+    private static final class NorRecursive implements Method {
+
+        public int kthSmallest(TreeNode root, int k) {
+            if (root == null) {
+                return -1;
+            }
+            int cursor = 0;
+            Stack<TreeNode> stack = new Stack<>();
+            while (root != null || !stack.isEmpty()) {
+                if (root != null) {
+                    stack.push(root);
+                    root = root.left;
+                } else {
+                    root = stack.pop();
+                    if (++cursor == k) {
+                        return root.val;
+                    }
+                }
+            }
+            return -1;
+        }
     }
-    
-    return stack.peek().val;
-  }
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    private static final class NorRecursiveIterator implements Method {
 
-  }
+        public int kthSmallest(TreeNode root, int k) {
+            Stack<TreeNode> stack = new Stack<>();
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
 
+            for (int i = 0; i < k - 1; i++) {
+                TreeNode cursor = stack.pop();
+                TreeNode right = cursor.right;
+                while (right != null) {
+                    stack.push(right);
+                    right = right.left;
+                }
+            }
+
+            return stack.peek().val;
+        }
+    }
 }
