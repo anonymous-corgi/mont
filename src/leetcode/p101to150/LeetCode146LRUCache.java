7961952f4c2650b019748fc407733679c4db8879
Ray Huang <huang.jingze@columbia.edu>
2019-10-05 00:10:39 -0700

    [Mod] LeetCode146LRUCache

--- src/leetcode/p101to150/LeetCode146LRUCache.java
+++ src/leetcode/p101to150/LeetCode146LRUCache.java
@@ -6,146 +6,144 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LeetCode146LRUCache {
-  
-  interface LRUCache {
-    int get(int key);
-    void put(int key, int value);
-  }
-  
-  public static class DoubleLinkedList_method implements LRUCache {
-    
-    private static class Node {
-      int key;
-      int val;
-      Node next;
-      Node prev;
-      public Node(int key, int val) {
-        this.key = key;
-        this.val = val;
-      }
+
+    private interface LRUCache {
+
+        int get(int key);
+
+        void put(int key, int value);
     }
-    
-    private final int CAPACITY;
-    private final Node HEAD;
-    private final Node END;
-    private final Map<Integer, Node> MAP;
-    private int size = 0;
-    
-    public DoubleLinkedList_method(int capacity) {
-      this.CAPACITY = capacity;
-      HEAD = new Node(0, -1);
-      END = new Node(0, -1);
-      HEAD.next = END;
-      END.prev = HEAD;
-      MAP = new HashMap<>();
-    }
-    
-    public int get(int key) {
-      if (!MAP.containsKey(key)) {
-        return -1;
-      }
-      Node cursor = MAP.get(key);
-      if (cursor.prev != HEAD) {
-        addToFirst(extract(cursor));
-      }
-      return cursor.val;
-    }
-    
-    public void put(int key, int value) {
-      if (MAP.containsKey(key)) {
-        Node cursor = MAP.get(key);
-        if (cursor.prev != HEAD) {
-          addToFirst(extract(cursor));
+
+    public static final class DoubleLinkedList implements LRUCache {
+
+        private static class Node {
+            int key;
+            int val;
+            Node next;
+            Node prev;
+
+            Node(int key, int val) {
+                this.key = key;
+                this.val = val;
+            }
         }
-        cursor.val = value;
-      } else {
-        Node cursor = new Node(key, value);
-        addToFirst(cursor);            
-        MAP.put(key, cursor);
-        size++;
-      }      
-      if (size > CAPACITY) {
-        Node rm = extract(END.prev);
-        MAP.remove(rm.key);
-        size--;
-      }
-    }
-    
-    private void addToFirst(Node node) {
-      node.next = HEAD.next;
-      HEAD.next.prev = node;
-      node.prev = HEAD;
-      HEAD.next = node;
-    }
-    
-    private Node extract(Node node) {
-      node.prev.next = node.next;
-      node.next.prev = node.prev;
-      node.next = null;
-      node.prev = null;
-      return node;
-    }
-    
-  }
-  
-  
-  public static class LinkedHashMap_method1 implements LRUCache {
-    
-    private final int CAPACITY;
-    private final LinkedHashMap<Integer,Integer> map;
-    
-    public LinkedHashMap_method1(int capacity) {
-      this.CAPACITY = capacity;
-      this.map = new LinkedHashMap<>();
-    }
-    
-    public int get(int key) {
-      Integer value = this.map.get(key);
-      if (value == null) {
-        value = -1;
-      } else {
-        this.put(key, value);
-      }
-      return value;
-    }
-    
-    public void put(int key, int value) {
-      if (this.map.containsKey(key)) {
-        this.map.remove(key);
-      } else if (this.map.size() == this.CAPACITY) {
-        Iterator<Integer> it = this.map.keySet().iterator();
-        it.next();
-        it.remove();
-      }
-      map.put(key, value);
-    }
-    
-  }
-  
-  
-  public static class LinkedHashMap_method2 implements LRUCache {
-    
-    private final int CAPACITY;
-    private final LinkedHashMap<Integer,Integer> map;
-    
-    @SuppressWarnings("serial")
-    public LinkedHashMap_method2(int capacity) {
-      this.CAPACITY = capacity;
-      this.map =  new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
-        protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-          return size() > CAPACITY;
+
+        private final int CAPACITY;
+        private final Node HEAD;
+        private final Node TAIL;
+        private final Map<Integer, Node> MAP;
+
+        public DoubleLinkedList(int capacity) {
+            this.CAPACITY = capacity;
+            HEAD = new Node(0, -1);
+            TAIL = new Node(0, -1);
+            HEAD.next = TAIL;
+            TAIL.prev = HEAD;
+            MAP = new HashMap<>();
         }
-      };
+
+        public int get(int key) {
+            Node cursor = MAP.get(key);
+            if (cursor == null) {
+                return -1;
+            } else if (cursor.prev != HEAD) {
+                extract(cursor);
+                addToFirst(cursor);
+            }
+            return cursor.val;
+        }
+
+        public void put(int key, int value) {
+            Node cursor = MAP.get(key);
+            if (cursor == null) {
+                cursor = new Node(key, value);
+                addToFirst(cursor);
+                MAP.put(key, cursor);
+            } else {
+                if (cursor.prev != HEAD) {
+                    extract(cursor);
+                    addToFirst(cursor);
+                }
+                cursor.val = value;
+            }
+            if (MAP.size() > CAPACITY) {
+                Node staleNode = TAIL.prev;
+                extract(staleNode);
+                MAP.remove(staleNode.key);
+            }
+        }
+
+        private void addToFirst(Node node) {
+            insert(node, HEAD);
+        }
+
+        private void insert(Node node, Node front) {
+            front.next.prev = node;
+            node.next = front.next;
+            front.next = node;
+            node.prev = front;
+        }
+
+        private void extract(Node node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            node.next = null;
+            node.prev = null;
+        }
     }
-    
-    public int get(int key) {
-      return map.getOrDefault(key, -1);
+
+    public static final class LinkedHashMap1 implements LRUCache {
+
+        private final int CAPACITY;
+        private final LinkedHashMap<Integer, Integer> map;
+
+        public LinkedHashMap1(int capacity) {
+            this.CAPACITY = capacity;
+            this.map = new LinkedHashMap<>();
+        }
+
+        public int get(int key) {
+            Integer value = this.map.get(key);
+            if (value == null) {
+                value = -1;
+            } else {
+                this.put(key, value);
+            }
+            return value;
+        }
+
+        public void put(int key, int value) {
+            if (this.map.containsKey(key)) {
+                this.map.remove(key);
+            } else if (this.map.size() == this.CAPACITY) {
+                Iterator<Integer> it = this.map.keySet().iterator();
+                it.next();
+                it.remove();
+            }
+            map.put(key, value);
+        }
     }
-    
-    public void put(int key, int value) {
-      map.put(key, value);
+
+    public static final class LinkedHashMap2 implements LRUCache {
+
+        private final int CAPACITY;
+        private final LinkedHashMap<Integer, Integer> map;
+
+        public LinkedHashMap2(int capacity) {
+            this.CAPACITY = capacity;
+            this.map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+                protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                    return size() > CAPACITY;
+                }
+            };
+        }
+
+        public int get(int key) {
+            return map.getOrDefault(key, -1);
+        }
+
+        public void put(int key, int value) {
+            map.put(key, value);
+        }
     }
-    
-  }
-  
 }
