abdaf00952e1e7150050135a3908d3961413f76a
Ray Huang <huang.jingze@columbia.edu>
2019-10-08 15:54:01 -0700

    [Mod] LeetCode621TaskScheduler

--- src/leetcode/p601to650/LeetCode621TaskScheduler.java
+++ src/leetcode/p601to650/LeetCode621TaskScheduler.java
@@ -3,26 +3,29 @@ package leetcode.p601to650;
 import java.util.Arrays;
 
 public class LeetCode621TaskScheduler {
-  
-  public int leastInterval(char[] tasks, int n) {
-    if (tasks == null || tasks.length == 0) {
-      return 0;
-    }
-    int[] cs = new int[26];
-    for (int i = 0; i < tasks.length; i++) {
-      cs[tasks[i] - 'A']++;
-    }
-    Arrays.sort(cs);
-    int i = 1;
-    while (i < 26 && cs[25 - i] == cs[25]) {
-      i++;
-    }
-    return Math.max(tasks.length, (n + 1) * (cs[25] - 1) + i);
-  }
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    private interface Method {
+        int leastInterval(char[] tasks, int n);
+    }
 
-  }
+    private static final class Trick implements Method {
 
+        public int leastInterval(char[] tasks, int n) {
+            if (tasks.length == 0) {
+                return 0;
+            }
+            int[] repetitions = new int[26];
+            for (char task : tasks) {
+                repetitions[task - 'A']++;
+            }
+            Arrays.sort(repetitions);
+            int countMaxRepetition = 25;
+            while (countMaxRepetition >= 0 && repetitions[countMaxRepetition] == repetitions[25]) {
+                countMaxRepetition--;
+            }
+            countMaxRepetition = 25 - countMaxRepetition;
+
+            return Math.max(tasks.length, (n + 1) * (repetitions[25] - 1) + countMaxRepetition);
+        }
+    }
 }
