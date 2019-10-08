176d94748f232ea1eab98accf76c0ece2fe410ea
Ray Huang <huang.jingze@columbia.edu>
2019-10-08 15:29:47 -0700

    [Mod] LeetCode124BinaryTreeMaximumPathSum

--- src/leetcode/p101to150/LeetCode124BinaryTreeMaximumPathSum.java
+++ src/leetcode/p101to150/LeetCode124BinaryTreeMaximumPathSum.java
@@ -3,27 +3,27 @@ package leetcode.p101to150;
 import basicclass.TreeNode;
 
 public class LeetCode124BinaryTreeMaximumPathSum {
-  
-  private int max = Integer.MIN_VALUE;
-  
-  public int maxPathSum(TreeNode root) {
-    helper(root);
-    return max;
-  }
-  
-  private int helper(TreeNode root) {
-    if (root == null) {
-      return 0;
+
+    private interface Method {
+        int maxPathSum(TreeNode root);
     }
-    int lMax = helper(root.left);
-    int rMax = helper(root.right);
-    max = Math.max(max, lMax + rMax + root.val);
-    
-    return Math.max(0, (lMax > rMax ? lMax : rMax) + root.val);
-  }
 
-  public static void main(String[] args) {
+    private static final class Traversal implements Method {
 
-  }
+        public int maxPathSum(TreeNode root) {
+            int[] res = new int[]{root.val};
+            maxPathSum(root, res);
+            return res[0];
+        }
 
+        private int maxPathSum(TreeNode root, int[] res) {
+            if (root == null) {
+                return 0;
+            }
+            int lMax = maxPathSum(root.left, res);
+            int rMax = maxPathSum(root.right, res);
+            res[0] = Math.max(res[0], root.val + lMax + rMax);
+            return Math.max(0, root.val + Math.max(lMax, rMax));
+        }
+    }
 }
