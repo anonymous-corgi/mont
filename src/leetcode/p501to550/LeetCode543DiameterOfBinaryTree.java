7d886f251d95c4d32dad5b199304ef8b29bf41c0
Ray Huang <huang.jingze@columbia.edu>
2019-10-08 17:09:55 -0700

    [Mod] LeetCode543DiameterOfBinaryTree

--- src/leetcode/p501to550/LeetCode543DiameterOfBinaryTree.java
+++ src/leetcode/p501to550/LeetCode543DiameterOfBinaryTree.java
@@ -9,31 +9,8 @@ public class LeetCode543DiameterOfBinaryTree {
         int diameterOfBinaryTree(TreeNode root);
     }
 
-    private static class Traversal implements Method {
-
-        private int maxLen = 0;
-
-        @Override
-        public int diameterOfBinaryTree(TreeNode root) {
-            maxLen = 0;
-            helper(root);
-            return maxLen;
-        }
-
-        private int helper(TreeNode root) {
-            if (root == null) {
-                return 0;
-            }
-            int left = helper(root.left);
-            int right = helper(root.right);
-            maxLen = Math.max(maxLen, left + right);
-            return Math.max(left, right) + 1;
-        }
-    }
-
     private static class BackTracking implements Method {
 
-        @Override
         public int diameterOfBinaryTree(TreeNode root) {
             return helper(root)[1];
         }
