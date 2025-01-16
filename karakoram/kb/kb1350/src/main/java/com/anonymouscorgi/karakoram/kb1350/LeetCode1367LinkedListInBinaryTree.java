b91e52d8f4783315891abe694fddf409e55dca5c
Corgi <github.com@ny13corgi.com>
2025-01-15 19:15:20 -0800

    Add LeetCode1367LinkedListInBinaryTree

---
+++ karakoram/kb/kb1350/src/main/java/com/anonymouscorgi/karakoram/kb1350/LeetCode1367LinkedListInBinaryTree.java
@@ -0,0 +1,33 @@
+package com.anonymouscorgi.karakoram.kb1350;
+
+import com.anonymouscorgi.karakoram.base.ListNode;
+import com.anonymouscorgi.karakoram.base.TreeNode;
+
+interface LeetCode1367LinkedListInBinaryTree {
+
+  boolean isSubPath(ListNode head, TreeNode root);
+
+  LeetCode1367LinkedListInBinaryTree METHOD = new LeetCode1367LinkedListInBinaryTree() {
+    @Override
+    public boolean isSubPath(ListNode head, TreeNode root) {
+      return isSubPath(head, head, root);
+    }
+
+    private static boolean isSubPath(ListNode head, ListNode next, TreeNode node) {
+      if (node == null) {
+        return false;
+      }
+      if (node.val == next.val) {
+        next = next.next;
+        if (next == null || isSubPath(null, next, node.left) || isSubPath(null, next, node.right)) {
+          return true;
+        }
+      }
+      if (head != null) {
+        return isSubPath(head, head, node.left) || isSubPath(head, head, node.right);
+      } else {
+        return false;
+      }
+    }
+  };
+}
