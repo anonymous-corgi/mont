dc7875dc4de8fecc89c38c96dda4f7c878030524
Corgi <github.com@ny13corgi.com>
2025-03-13 23:15:55 -0700

    Add LeetCode1047RemoveAllAdjacentDuplicatesInString

---
+++ karakoram/kb/kb1000/src/main/java/com/anonymouscorgi/karakoram/kb1000/LeetCode1047RemoveAllAdjacentDuplicatesInString.kt
@@ -0,0 +1,26 @@
+package com.anonymouscorgi.karakoram.kb1000
+
+import com.anonymouscorgi.karakoram.annotation.Accepted
+import com.anonymouscorgi.karakoram.annotation.Easy
+
+@Easy
+interface LeetCode1047RemoveAllAdjacentDuplicatesInString {
+
+    fun removeDuplicates(s: String): String
+
+    @Accepted
+    object METHOD : LeetCode1047RemoveAllAdjacentDuplicatesInString {
+
+        override fun removeDuplicates(s: String): String {
+            val stack = ArrayDeque<Char>()
+            for (char in s) {
+                if (stack.isNotEmpty() && stack.last() == char) {
+                    stack.removeLast()
+                } else {
+                    stack.addLast(char)
+                }
+            }
+            return stack.joinToString("")
+        }
+    }
+}
