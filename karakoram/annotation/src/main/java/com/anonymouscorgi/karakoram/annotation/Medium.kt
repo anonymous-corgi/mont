3993536edbff9718f78b903b801f93f7f6dabbaf
Corgi <github.com@ny13corgi.com>
2025-03-13 20:22:33 -0700

    Convert annotation to kotlin

---
+++ karakoram/annotation/src/main/java/com/anonymouscorgi/karakoram/annotation/Medium.kt
@@ -0,0 +1,5 @@
+package com.anonymouscorgi.karakoram.annotation
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE, AnnotationTarget.TYPE_PARAMETER)
+@Retention(AnnotationRetention.SOURCE)
+annotation class Medium 
